b32054bc7a8af1e625e21af180e630f1fee22885
dmdrk1414 <seungchan141414@gmail.com>
2023-07-23 03:22:47 +0900

    [Feat] database connect complete

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/TestController.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/TestController.java
@@ -1,13 +1,25 @@
 package back.springbootdeveloper.seungchan.controller;
 
+import back.springbootdeveloper.seungchan.domain.Suggestions;
+import back.springbootdeveloper.seungchan.repository.SuggestionRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+   private final SuggestionRepository suggestionRepository;
+
+    public TestController(SuggestionRepository suggestionRepository) {
+        this.suggestionRepository = suggestionRepository;
+    }
 
     @GetMapping("/test")
     public String testController(){
+        suggestionRepository.save(Suggestions.builder()
+                .classification("test classification")
+                .title("test Title")
+                .check(true)
+                .build());
         return "Hello World";
     }
 }
