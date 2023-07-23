507a07c2b9ebf6ad212f9b9f8b95c3a29b86e28e
dmdrk1414 <seungchan141414@gmail.com>
2023-07-23 15:58:53 +0900

    [Feat] 건의 게시판 조회
    건의 게시판을 조회를 한다.
    suggestion db을 조회한다.

---
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/SuggestionService.java
@@ -0,0 +1,21 @@
+package back.springbootdeveloper.seungchan.service;
+
+
+import back.springbootdeveloper.seungchan.domain.Suggestions;
+import back.springbootdeveloper.seungchan.repository.SuggestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor // final이 붙거나  @NotNull이 붙는 필드의 생성자 추가
+@Service
+public class SuggestionService {
+    private final SuggestionRepository suggestionRepository;
+
+
+    public List<Suggestions> findAll() {
+        return suggestionRepository.findAll();
+    }
+}
