b0df74012b7961833b0d00fd0a8ace18e01dcef4
dmdrk1414 <seungchan141414@gmail.com>
2023-07-30 02:05:57 +0900

    [Feat] 6,7 메인-졸업자 출력 페이지 (일반, 실장) ob 추가
    졸업자을 출력을 한다.
    name yearOfRegistration phoneNum 의 정보를 출력

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/MainController.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/MainController.java
@@ -1,12 +1,15 @@
 package back.springbootdeveloper.seungchan.controller;
 
+import back.springbootdeveloper.seungchan.domain.ObUser;
 import back.springbootdeveloper.seungchan.domain.User;
 import back.springbootdeveloper.seungchan.domain.UserUtill;
+import back.springbootdeveloper.seungchan.dto.response.ObUserOfMainResponse;
 import back.springbootdeveloper.seungchan.dto.response.UserOfDetail2MainResponse;
 import back.springbootdeveloper.seungchan.dto.response.YbUserOfMainResponse;
 import back.springbootdeveloper.seungchan.repository.UserUtilRepository;
 import back.springbootdeveloper.seungchan.service.UserOfMainService;
 import back.springbootdeveloper.seungchan.service.UserService;
+import back.springbootdeveloper.seungchan.service.UserUtillService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -24,6 +28,7 @@ public class MainController {
     private final UserService userService;
     private final UserUtilRepository userUtilRepository;
     private final UserOfMainService userOfMainService;
+    private final UserUtillService userUtillService;
 
     ///main/ybs
     @GetMapping("main/ybs")
@@ -31,6 +36,21 @@ public class MainController {
         boolean isObUser = false;
         List<YbUserOfMainResponse> list = userOfMainService.findAllByIsOb(isObUser);
         return ResponseEntity.ok().body(list);
+    }
+
+
+    @GetMapping("main/obs")
+    public ResponseEntity<List<ObUserOfMainResponse>> findAllObUser() {
+        //         TODO Token을 받아서 현재 유저의 정보를 가져와야된다.
+        // 1. i will take token from front
+        // 2. i have to get a user Information from token of user
+        // 3. so... resulte that we get a user_ID from token
+        Long userTempID = Long.valueOf(1); // user_id is ID from user DB
+        boolean isNuriKing = userUtillService.isNuriKing(userTempID);
+
+        List<ObUser> obUserList = userOfMainService.findAllObUser();
+
+        return ResponseEntity.ok().body(Collections.singletonList(new ObUserOfMainResponse(obUserList, isNuriKing)));
     }
 
     @GetMapping("/main/detail")
