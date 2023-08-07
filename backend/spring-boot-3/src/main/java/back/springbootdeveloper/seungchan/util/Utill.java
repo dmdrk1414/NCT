f98065532e208b88a4795877c100c91674fe2416
dmdrk1414 <seungchan141414@gmail.com>
2023-08-08 08:43:52 +0900

    [Feat] request을 이용해 토큰을 얻는 방법

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/util/Utill.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/util/Utill.java
@@ -1,5 +1,8 @@
 package back.springbootdeveloper.seungchan.util;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.BadCredentialsException;
+
 import java.util.List;
 
 public class Utill {
@@ -9,5 +12,20 @@ public class Utill {
 
     public static List<String> arrFromStr(String str) {
         return List.of(str.split(", "));
+    }
+
+    public static String getToken(HttpServletRequest request) {
+        // HTTP Request에서 "Authorization" 헤더 값 얻기
+        String header = request.getHeader("Authorization");
+
+        // 토큰이 없는 경우나 "Bearer " 접두사를 포함하지 않은 경우 처리
+        if (header == null || !header.startsWith("Bearer ")) {
+            throw new BadCredentialsException("Invalid token");
+        }
+
+        // "Bearer " 접두사를 제거하여 실제 토큰 얻기
+        String token = header.replace("Bearer ", "");
+
+        return token;
     }
 }
