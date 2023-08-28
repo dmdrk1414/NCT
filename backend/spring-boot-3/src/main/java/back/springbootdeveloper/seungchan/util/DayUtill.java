1e868291b4993ca628d69dd209d99df2c8a6bf6a
박승찬 <seungchan141414@gmail.com>
2023-08-28 16:17:14 +0900

    [Refactor] 성훈이가 잠시 수정 해달라는 것들을 다시 원상 복귀

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/util/DayUtill.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/util/DayUtill.java
@@ -20,9 +20,7 @@ public class DayUtill {
         // 요일을 얻습니다.
         DayOfWeek dayOfWeek = date.getDayOfWeek();
 
-        // TODO: 8/27/23 다시 변경
-//        if (dayOfWeek != DayOfWeek.SATURDAY && dayOfWeek != DayOfWeek.SUNDAY) {
-        if (dayOfWeek != DayOfWeek.SATURDAY) {
+        if (dayOfWeek != DayOfWeek.SATURDAY && dayOfWeek != DayOfWeek.SUNDAY) {
             return dayOfWeek;
         }
         throw new IllegalArgumentException("토요일 일요일은 안됩니다.");
