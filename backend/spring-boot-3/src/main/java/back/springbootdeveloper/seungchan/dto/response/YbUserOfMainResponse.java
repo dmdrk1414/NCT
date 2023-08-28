0cd4b0e34e43b7f4fe98258cb4f8234811728e30
박승찬 <seungchan141414@gmail.com>
2023-08-28 16:35:52 +0900

    [Refactor] yb 회원들을 조회를 할때 이번주 출석 데이터를 리스트로 반환변경

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/response/YbUserOfMainResponse.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/response/YbUserOfMainResponse.java
@@ -2,10 +2,13 @@ package back.springbootdeveloper.seungchan.dto.response;
 
 import back.springbootdeveloper.seungchan.domain.AttendanceStatus;
 import back.springbootdeveloper.seungchan.domain.UserUtill;
+import back.springbootdeveloper.seungchan.util.Utill;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,14 +16,14 @@ import lombok.Setter;
 public class YbUserOfMainResponse {
     private String name;
     private int cntVacation;
-    private String weeklyData;
+    private List<Integer> weeklyData;
     private Long userId;
 
     @Builder
     public YbUserOfMainResponse(AttendanceStatus attendanceStatus, UserUtill userUtill) {
         this.name = userUtill.getName();
         this.cntVacation = userUtill.getCntVacation();
-        this.weeklyData = attendanceStatus.getWeeklyData();
+        this.weeklyData = Utill.extractNumbers(attendanceStatus.getWeeklyData());
         this.userId = userUtill.getUserId();
     }
 }
--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/util/Utill.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/util/Utill.java
@@ -3,7 +3,10 @@ package back.springbootdeveloper.seungchan.util;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.authentication.BadCredentialsException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utill {
     public static boolean isEqualStr(String arg1, String arg2) {
@@ -12,5 +15,24 @@ public class Utill {
 
     public static List<String> arrFromStr(String str) {
         return List.of(str.split(", "));
+    }
+
+    /**
+     * @param input "[1,1,1,1]" 의 문자열을 input
+     * @return input의 문자열을 숫자만 추출하여 리스트로 만든다.
+     */
+    public static List<Integer> extractNumbers(String input) {
+        List<Integer> integerList = new ArrayList<>();
+
+        // 정규 표현식을 사용하여 숫자 추출
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            // 추출된 문자열을 Integer로 변환하여 리스트에 추가
+            integerList.add(Integer.parseInt(matcher.group()));
+        }
+
+        return integerList;
     }
 }
