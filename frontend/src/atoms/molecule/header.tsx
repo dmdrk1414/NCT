bd350766af6999497296930478f83c3f73d05a80
박승찬 <seungchan141414@gmail.com>
2023-12-12 22:47:42 +0900

    [Feat: front] 개인휴가 신청을 할때 모달창 기능 추가

--- frontend/src/atoms/molecule/header.tsx
+++ frontend/src/atoms/molecule/header.tsx
@@ -3,12 +3,31 @@ import Link from 'next/link';
 import { useRecoilState } from 'recoil';
 import { userToken } from '../../states/index';
 import { axAuth } from '@/apis/axiosinstance';
+import AllertModal from '../../atoms/atom/allert-modal';
+import { useState, useEffect } from 'react';
+
 type Data = {
   isVisible: boolean;
 };
 
 export default function Header(Data: Data) {
   const [token, setToken] = useRecoilState(userToken);
+  const [AllertModalstatus, setAllertModalStatus] = useState(0);
+
+  useEffect(() => {
+    // 휴가 신청 모달창
+    if (AllertModalstatus !== 0) {
+      setTimeout(() => {
+        setAllertModalStatus(0); // 2초 후에 AllertModal 닫기
+      }, 1000);
+    }
+  }, [AllertModalstatus]);
+
+  const textOfAllert = [
+    // 휴가 신청 모달창 텍스트 입력
+    { title: '휴가 신청 완료', context: '카톡으로 실장에게 말씀하세요.', type: 'success' },
+    { title: '휴가 신청을 할수 없습니다.', context: '이미 휴가, 결석, 출석을 하였습니다.', type: 'danger' },
+  ];
 
   const useVacation = () => {
     axAuth(token)({
@@ -16,7 +35,15 @@ export default function Header(Data: Data) {
       url: '/vacations/request/each',
     })
       .then(res => {
-        console.log('휴가 사용 완료');
+        const availableApplyVacation = res.data.availableApplyVacation;
+
+        // 0이 닫기, 1이 성공, 2가 실패
+        if (availableApplyVacation) {
+          setAllertModalStatus(1);
+          console.log('휴가 사용 완료');
+        } else {
+          setAllertModalStatus(2);
+        }
       })
       .catch(err => {
         console.log(err);
@@ -25,6 +52,10 @@ export default function Header(Data: Data) {
 
   return (
     <div className="flex items-center py-[1.3rem] relative">
+      {AllertModalstatus !== 0 ? (
+        <AllertModal title={textOfAllert[AllertModalstatus - 1].title} context={textOfAllert[AllertModalstatus - 1].context} type={textOfAllert[AllertModalstatus - 1].type} />
+      ) : null}
+
       <div className="w-[8rem] h-[3rem] ms-[0.5rem]">
         <Link href={'/'}>
           <img className="w-[100%] h-[100%]" src="/Images/NCT_Logo.png" alt="로고 이미지" />
