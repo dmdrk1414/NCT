69fea2c91e221151018dfeef4239e04089ee9d3b
박승찬 <seungchan141414@gmail.com>
2023-12-12 17:55:03 +0900

    [Fix] 휴가 중복 사용 불가
    이전에는 휴가를 중복에서 사용, 결석과 출석을 하면 사용이 가능하였다.
    지금은 휴가를 중복 사용, 결석과 출석을 하면 불가능하다.
    
    휴가가 가능하면 true
    휴가가 불가능하면 false 이다.

---
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/response/AvailableApplyVacationResponse.java
@@ -0,0 +1,12 @@
+package back.springbootdeveloper.seungchan.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AvailableApplyVacationResponse {
+    private Boolean availableApplyVacation;
+}
