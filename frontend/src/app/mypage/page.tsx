60ac3ac0e59c378e982745c76b81759e043b37b2
박승찬 <seungchan141414@gmail.com>
2023-12-13 11:44:15 +0900

    [Feat : front] 마이페이지 마진 추가

--- frontend/src/app/mypage/page.tsx
+++ frontend/src/app/mypage/page.tsx
@@ -48,46 +48,47 @@ export default function SignUp() {
   }, []);
 
   const isLoginAndSignupButton = false;
-
   return (
     <main>
-      <header>
-        <Header isVisible={isLoginAndSignupButton} />
-      </header>
-      <article className="px-[7.5%]">
-        <div className="mypage-content-one">
-          <div className="text-3xl font-extrabold">내 정보</div>
-          <div className="flex items-center">
-            <div className="mr-3 bg-black w-20 h-20 rounded-full"></div>
-            <div className="font-bold">
-              <div>
-                이름(학번): {userData.name} ({userData.studentId})
+      <div className="mb-10">
+        <header>
+          <Header isVisible={isLoginAndSignupButton} />
+        </header>
+        <article className="px-[7.5%]">
+          <div className="mypage-content-one">
+            <div className="text-3xl font-extrabold">내 정보</div>
+            <div className="flex items-center">
+              <div className="mr-3 bg-black w-20 h-20 rounded-full"></div>
+              <div className="font-bold">
+                <div>
+                  이름(학번): {userData.name} ({userData.studentId})
+                </div>
+                <div>
+                  학과(학점): {userData.major} ({userData.gpa})
+                </div>
+                <div>연락처: {userData.phoneNum}</div>
+                <div>주소: {userData.address}</div>
               </div>
-              <div>
-                학과(학점): {userData.major} ({userData.gpa})
-              </div>
-              <div>연락처: {userData.phoneNum}</div>
-              <div>주소: {userData.address}</div>
             </div>
           </div>
-        </div>
-        <div>
-          <div className="mt-2">MBTI: {userData.mbti}</div>
-          <div className="mt-2">특기: {userData.specialtySkill}</div>
-          <div className="mt-2">생년월일: {userData.birthDate}</div>
-          <div className="mt-2">취미: {userData.hobby}</div>
-          <div className="mt-2">장점: {userData.advantages}</div>
-          <div className="mt-2">단점: {userData.disadvantage}</div>
-          <div className="mt-2">자기소개: {userData.selfIntroduction}</div>
+          <div>
+            <div className="mt-2">MBTI: {userData.mbti}</div>
+            <div className="mt-2">특기: {userData.specialtySkill}</div>
+            <div className="mt-2">생년월일: {userData.birthDate}</div>
+            <div className="mt-2">취미: {userData.hobby}</div>
+            <div className="mt-2">장점: {userData.advantages}</div>
+            <div className="mt-2">단점: {userData.disadvantage}</div>
+            <div className="mt-2">자기소개: {userData.selfIntroduction}</div>
 
-          <a href="https://www.nuri777.kro.kr/mypage/update">
-            <div className="mt-2 font-bold">나의 페이지 수정하기 이동</div>
-          </a>
-        </div>
-      </article>
-      <div className="mt-[3rem]">
-        <div>
-          <SubmitButton text={'로그아웃'} addClass={'bg-red'} />
+            <a onClick={() => router.push('/mypage/update')}>
+              <SubmitButton text={'나의 페이지 수정하기'} addClass="text-2xl mt-5" />
+            </a>
+          </div>
+        </article>
+        <div className="mt-[3rem]">
+          <div>
+            <SubmitButton text={'로그아웃'} addClass={'bg-red mb-5'} />
+          </div>
         </div>
       </div>
 
