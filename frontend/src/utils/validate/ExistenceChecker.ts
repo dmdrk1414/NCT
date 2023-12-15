de240b8cdddcbc7c617ef1cdf5d355285194a76e
박승찬 <seungchan141414@gmail.com>
2023-12-15 23:28:43 +0900

    [Feat : front] 토큰이 존제하지 않을 경우 검증 메서드 추가

--- frontend/src/utils/validate/ExistenceChecker.ts
+++ frontend/src/utils/validate/ExistenceChecker.ts
@@ -6,3 +6,12 @@
 export function hasToken(token: string): boolean {
   return token !== null && token !== undefined && token !== '';
 }
+
+/**
+ * token여부 판변
+ * @param token
+ * @returns 없으면 true
+ */
+export function hasNotToken(token: string): boolean {
+  return !hasToken(token);
+}
