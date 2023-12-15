106bb8282784c04fe68b7701b135a3db1ce2a1a1
박승찬 <seungchan141414@gmail.com>
2023-12-15 16:36:45 +0900

    [Feat : front] 오성훈 휴가 신청 모달 위치 수정
    헤더 전체의 div와 동일한 위치로 이동

--- frontend/src/atoms/molecule/header.tsx
+++ frontend/src/atoms/molecule/header.tsx
@@ -51,28 +51,29 @@ export default function Header(Data: Data) {
   };
 
   return (
-    <div className="flex items-center py-[1.3rem] relative">
+    <>
       {AllertModalstatus !== 0 ? (
         <AllertModal title={textOfAllert[AllertModalstatus - 1].title} context={textOfAllert[AllertModalstatus - 1].context} type={textOfAllert[AllertModalstatus - 1].type} />
       ) : null}
-
-      <div className="w-[8rem] h-[3rem] ms-[0.5rem]">
-        <Link href={'/'}>
-          <img className="w-[100%] h-[100%]" src="/Images/NCT_Logo.png" alt="로고 이미지" />
-        </Link>
-      </div>
-      {Data.isVisible === true ? (
-        <Link href="/login" className="absolute right-[0.5rem]">
-          <MiddleButton addClass="text-xl" text="로그인/지원서 작성" />
-        </Link>
-      ) : null}
-      {token ? (
-        <div className="absolute right-[0.5rem]">
-          <div onClick={useVacation}>
-            <MiddleButton addClass="text-xl" text="휴가 사용하기" />
-          </div>
+      <div className="flex items-center py-[1.3rem]">
+        <div className="w-[8rem] h-[3rem] ms-[0.5rem]">
+          <Link href={'/'}>
+            <img className="w-[100%] h-[100%]" src="/Images/NCT_Logo.png" alt="로고 이미지" />
+          </Link>
         </div>
-      ) : null}
-    </div>
+        {Data.isVisible === true ? (
+          <Link href="/login" className="absolute right-[0.5rem]">
+            <MiddleButton addClass="text-xl" text="로그인/지원서 작성" />
+          </Link>
+        ) : null}
+        {token ? (
+          <div className="absolute right-[0.5rem]">
+            <div onClick={useVacation}>
+              <MiddleButton addClass="text-xl" text="휴가 사용하기" />
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </>
   );
 }
