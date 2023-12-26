f86600d95776adee2ec0f8cacc1960cffa46a4c5
박승찬 <seungchan141414@gmail.com>
2023-12-26 21:07:54 +0900

    [Test : back] 유저_로그인_테스트_예외_테스트_형식에_맞지_않는_이메일

--- backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/LoginPageControllerTest.java
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/LoginPageControllerTest.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -41,6 +42,8 @@ class LoginPageControllerTest {
     private String MESSAGE_EMAIL_NOT_NULL;
     @Value("${password.notnull}")
     private String MESSAGE_PASSWORD_NOT_NULL;
+    @Value("${email.invalid}")
+    private String MESSAGE_EMAIL_INVALID;
 
     @BeforeEach
     public void setUp() {
@@ -107,6 +110,55 @@ class LoginPageControllerTest {
         HttpStatus httpStatus = TestUtills.getHttpStatusFromResponse(response);
 
         assertThat(message).isEqualTo(MESSAGE_PASSWORD_NOT_NULL);
+        assertThat(httpStatus).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "asdf",
+            "12  aea3",
+            "asdf sadf asdfdsa",
+            "sadf@sdaf@sdf",
+            "23!saf",
+            "invalid_email",
+            "user@.com",
+            "@example.com",
+            "user@.com.",
+            "user@example..com",
+            "user@exa mple.com",
+            "user@example.com.",
+            "user@.example.com",
+            "user@-example.com",
+            "user@example-.com",
+            "user@example.com-",
+            "user@exam@ple.com"
+    })
+    void 유저_로그인_테스트_예외_테스트_형식에_맞지_않는_이메일(String email) throws Exception {
+        String password = kingUser.getPassword();
+
+        // given
+        UserLoginRequest userLoginRequest = UserLoginRequest.builder()
+                .email(email)
+                .password(password)
+                .build();
+
+        final String url = "/login";
+
+        // when
+        final String requestBody = objectMapper.writeValueAsString(userLoginRequest);
+
+        MvcResult result = mockMvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody))
+                .andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+
+        // JSON 응답을 Map으로 변환
+        String message = TestUtills.getMessageFromResponse(response);
+        HttpStatus httpStatus = TestUtills.getHttpStatusFromResponse(response);
+
+        assertThat(message).isEqualTo(MESSAGE_EMAIL_INVALID);
         assertThat(httpStatus).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 }
