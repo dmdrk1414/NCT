4633fd4266c03cc576cfd6b5c753f8744b57f4bb
박승찬 <seungchan141414@gmail.com>
2023-12-26 21:11:09 +0900

    [Refactor : back] 상수 변경

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/Constant/filter/exception/ExceptionMessage.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/Constant/filter/exception/ExceptionMessage.java
@@ -3,11 +3,8 @@ package back.springbootdeveloper.seungchan.Constant.filter.exception;
 import lombok.Getter;
 
 public enum ExceptionMessage {
-    EMPTY_VALUE_EXCEPTION("양식에 빈 값이 있습니다."),
-    PASSWORD_NOT_MATCH_MESSAGE("비밀번호가 일치하지 않습니다."),
-    USER_ALREADY_EXIST_MESSAGE("동일한 이메일을 가진 유저가 이미 존재합니다."),
-    USER_NOT_EXIST_MESSAGE("유저가 존재하지 않습니다."),
-    LOG_IN_FAIL_MESSAGE("로그인에 실패하였습니다.");
+    USER_NOT_EXIST_MESSAGE("해당 사용자가 존재하지 않습니다."),
+    LOG_IN_FAIL_MESSAGE("로그인에 실패하였습니다. 다시 시도해주세요.");
 
     private final String message;
 
