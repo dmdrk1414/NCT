65dcc73128772ba2592bdfcc83f8edc7c7b01314
박승찬 <seungchan141414@gmail.com>
2023-12-26 22:38:18 +0900

    [Feat : back] properties 파일 인코딩 문제 해결
    properties 파일의 한글깨짐 문제 발생
    Transparent native -to-ascii conversion
    의 설정을 하였다

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/config/webconfig/WebConfig.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/config/webconfig/WebConfig.java
@@ -8,13 +8,17 @@ import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Locale;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Bean
     public MessageSource validationMessageSource() {
         // MessageSource를 정의하는 곳에 추가적으로 validationMessageSource
+        Locale.setDefault(Locale.KOREA); // 위치 한국으로 설정 (한국어로 에러 메세지 나오게)
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:/messages/validation/validation");
+//        messageSource.setBasenames("classpath:message/security_message", "classpath:org/springframework/security/messages"); // 커스텀한 properties 파일, security properties 파일 순서대로 설정
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
