ecbcccc039868029c83585d175713497e02f9b77
박승찬 <seungchan141414@gmail.com>
2023-12-29 19:58:34 +0900

    [Test : back] 테스트를 위한 기본설정추가

--- backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/AttendanceControllerTest.java
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/AttendanceControllerTest.java
@@ -1,15 +1,31 @@
 package back.springbootdeveloper.seungchan.controller;
 
+import back.springbootdeveloper.seungchan.dto.request.AttendanceNumberReqDto;
+import back.springbootdeveloper.seungchan.entity.NumOfTodayAttendence;
+import back.springbootdeveloper.seungchan.entity.UserInfo;
 import back.springbootdeveloper.seungchan.service.DatabaseService;
+import back.springbootdeveloper.seungchan.service.NumOfTodayAttendenceService;
+import back.springbootdeveloper.seungchan.testutills.TestSetUp;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest()
 @AutoConfigureMockMvc
@@ -20,13 +36,15 @@ class AttendanceControllerTest {
     protected ObjectMapper objectMapper;
     @Autowired
     private DatabaseService databaseService;
+    @Autowired
+    private TestSetUp testSetUp;
+    private String token;
 
     @BeforeEach
     void setUp() {
         databaseService.deleteAllDatabase();
+        token = testSetUp.getToken(mockMvc);
     }
 
-    @Test
-    void attendanceIsPassController() {
-    }
+
 }
