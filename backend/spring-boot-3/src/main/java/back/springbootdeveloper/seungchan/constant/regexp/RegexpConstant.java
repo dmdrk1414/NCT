ab8d5e69471244394aa8fbedbd25ba9360e94a5f
박승찬 <seungchan141414@gmail.com>
2023-12-29 20:41:30 +0900

    [Docs : back] 주석 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/constant/regexp/RegexpConstant.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/constant/regexp/RegexpConstant.java
@@ -1,5 +1,8 @@
 package back.springbootdeveloper.seungchan.constant.regexp;
 
+/**
+ * 정규식을 관리하는 enum
+ */
 public enum RegexpConstant {
     PASSWORD("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$");
 
