1766bc2742d255bdbc99f605e180180ad6f7b79c
박승찬 <seungchan141414@gmail.com>
2023-12-30 00:02:53 +0900

    [Feat : back] 출석 번호 입력에 따른 검증 추가
    @Pattern(regexp = "^[0-9]+$", message = "{validation.attendanceNumber.invalid}")
    숫자 검증을 추가하였다

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/request/AttendanceNumberReqDto.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/request/AttendanceNumberReqDto.java
@@ -1,6 +1,7 @@
 package back.springbootdeveloper.seungchan.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -11,5 +12,6 @@ import lombok.*;
 public class AttendanceNumberReqDto {
     @NotBlank(message = "{validation.attendanceNumber.notblank}")
     @Size(min = 4, message = "{validation.attendanceNumber.size.min.4}")
+    @Pattern(regexp = "^[0-9]+$", message = "{validation.attendanceNumber.invalid}")
     private String numOfAttendance;
 }
