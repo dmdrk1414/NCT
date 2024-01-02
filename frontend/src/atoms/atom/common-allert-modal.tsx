9b165afe4c1a4b9ecf4240ab53cd0b790edf5d7a
박승찬 <seungchan141414@gmail.com>
2024-01-02 15:53:31 +0900

    [Feat : front] 공통 모달 관련 컴포넌트 생성
    response에서 응답 메세지를 이용한 modal 이용

---
+++ frontend/src/atoms/atom/common-allert-modal.tsx
@@ -0,0 +1,29 @@
+import { HTTP_BAD_REQUEST, HTTP_STATUS_OK } from '@/utils/constans/httpStatus';
+
+type data = {
+  title: string;
+  context: string;
+  type: string;
+};
+
+export default function CommonAltertModal({ title, context, type }: data) {
+  return (
+    <div className="absolute z-10 w-[100vw] h-[100vh] bg-black/60 flex justify-center items-center">
+      {type === HTTP_BAD_REQUEST ? (
+        <>
+          <div className="bg-[#FFC3C3] w-[80%] h-[15%] rounded-[7%] flex flex-col justify-center items-center" onClick={e => e.stopPropagation()}>
+            <div className="text-center font-bold text-2xl text-red">{title}</div>
+            <div className="text-center font-semibold text-lg text-dark-grey mt-[0.5rem]">{context}</div>
+          </div>
+        </>
+      ) : type === HTTP_STATUS_OK ? (
+        <>
+          <div className="bg-white w-[80%] h-[15%] rounded-[7%] flex flex-col justify-center items-center" onClick={e => e.stopPropagation()}>
+            <div className="text-center font-bold text-2xl">{title}</div>
+            <div className="text-center font-semibold text-lg text-dark-grey mt-[0.5rem]">{context}</div>
+          </div>
+        </>
+      ) : null}
+    </div>
+  );
+}
