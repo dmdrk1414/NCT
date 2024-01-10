f9b62901d5b13140e32db29ed186c5caac446e19
박승찬 <seungchan141414@gmail.com>
2024-01-11 00:12:16 +0900

    [Feat: front] 건의 게시판의 매우긴 글을 10이상은 ...으로 표기

--- frontend/src/atoms/molecule/suggestion-article.tsx
+++ frontend/src/atoms/molecule/suggestion-article.tsx
@@ -4,6 +4,7 @@ import { axAuth } from '@/apis/axiosinstance';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { replaceRouterEachSuggestion } from '@/utils/RouteHandling';
+import Link from 'next/link';
 
 type Data = {
   id: number;
@@ -22,6 +23,7 @@ export default function SuggestionArticle(data: Data) {
   const numberCss = `w-[20%] flex ${justify} `;
   const classificationCss = `w-[18%] ${justify} `;
   const titleCss = `w-[72%] ${justify} `;
+  const titleLen = 10;
 
   const pushCheck = (id: number) => {
     if (isKing) {
@@ -45,8 +47,16 @@ export default function SuggestionArticle(data: Data) {
     <>
       <div className={`${numberCss}`}>{data.id}</div>
       <div className={`${classificationCss}`}>{data.classification}</div>
-      <div className={`${titleCss} flex justify-between ml-[0.2rem]`} onClick={() => replaceRouterEachSuggestion(router, data.id)}>
-        {data.title.length <= 15 ? data.title : data.title.substring(0, 15) + '...'}
+      <div className={`${titleCss} flex justify-between ml-[0.2rem]`}>
+        <span
+          onClick={() => replaceRouterEachSuggestion(router, data.id)}
+          style={{
+            cursor: 'pointer', // 포인터 모양 커서를 추가하여 클릭 가능성을 나타냅니다.
+            transition: 'background-color 0.3s', // 배경색 변경에 대한 부드러운 전환을 설정합니다.
+          }}
+        >
+          {data.title.length <= titleLen ? data.title : data.title.substring(0, titleLen) + '...'}
+        </span>
         <input className="w-[1.1rem] mr-[0.2rem]" type="checkbox" checked={data.check} onChange={() => pushCheck(data.id)} id="myCheckbox" />
       </div>
     </>
