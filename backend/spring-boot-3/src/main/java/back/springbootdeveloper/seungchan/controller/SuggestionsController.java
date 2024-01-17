0148739a142ded83b112af15ea62d395da6f80b6
박승찬 <seungchan141414@gmail.com>
2024-01-17 23:26:57 +0900

    [Refactor : back] 사용하지 않는 변수 삭제

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/SuggestionsController.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/SuggestionsController.java
@@ -31,7 +31,7 @@ public class SuggestionsController {
     @Operation(summary = "건의 게시판 작성", description = "개인 회원이 건의 게시판을 작성을 한다.")
     @PostMapping("/write")
     public ResponseEntity<BaseResponseBody> writeSuggestion(@RequestBody SuggestionWriteReqDto suggestionWriteRequest) {
-        Suggestions suggestions = suggestionService.save(suggestionWriteRequest);
+        suggestionService.save(suggestionWriteRequest);
 
         return BaseResponseBodyUtiil.BaseResponseBodySuccess();
     }
