b01f7a729e930eabc3dd17ab1c435fd3723b2d34
박승찬 <seungchan141414@gmail.com>
2024-01-17 23:14:47 +0900

    [Feat : back] 건의 게시판 관련 상수 enum관리 추가

---
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/constant/SuggestionConstant.java
@@ -0,0 +1,17 @@
+package back.springbootdeveloper.seungchan.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum SuggestionConstant {
+    SUGGESTION("건의"),
+    SECRET("비밀"),
+    FREEDOM("자유"),
+    VACATION("휴가");
+
+    private String classification;
+
+    SuggestionConstant(String classification) {
+        this.classification = classification;
+    }
+}
