ada5738f6c1f52eb1940fb52e63b9c89194affde
박승찬 <seungchan141414@gmail.com>
2024-01-17 23:18:58 +0900

    [Dcos : back] 주석 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/SuggestionsController.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/SuggestionsController.java
@@ -28,7 +28,7 @@ public class SuggestionsController {
     private final TokenService tokenService;
     private final UserUtillService userUtillService;
 
-    @Operation(summary = "건의 게시판 작성", description = "건의 게시판을 작성을 한다. 건의, 비밀, 자유, 휴가의 분류가 있으며 비밀은 실장만 조회가 가능하다.")
+    @Operation(summary = "건의 게시판 작성", description = "개인 회원이 건의 게시판을 작성을 한다.")
     @PostMapping("/write")
     public ResponseEntity<BaseResponseBody> writeSuggestion(@RequestBody SuggestionWriteRequest suggestionWriteRequest) {
         Suggestions suggestions = suggestionService.save(suggestionWriteRequest);
