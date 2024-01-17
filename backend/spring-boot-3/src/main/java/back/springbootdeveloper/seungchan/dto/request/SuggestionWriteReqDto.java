cb55ee4791594b28964427111c821b3c687b12fb
박승찬 <seungchan141414@gmail.com>
2024-01-18 00:00:31 +0900

    [Feat : back] 건의 게시판 작성에 대한 검증 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/request/SuggestionWriteReqDto.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/request/SuggestionWriteReqDto.java
@@ -1,6 +1,8 @@
 package back.springbootdeveloper.seungchan.dto.request;
 
 import back.springbootdeveloper.seungchan.entity.Suggestions;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @AllArgsConstructor
@@ -8,8 +10,13 @@ import lombok.*;
 @Data
 @Builder
 public class SuggestionWriteReqDto {
+    @NotBlank(message = "{validation.suggestion.classification.notblank}")
+    // controller에서 추가 검증 체크
     private String classification;
+
+    @NotBlank(message = "{validation.suggestion.title.notblank}")
     private String title;
+
     private String holidayPeriod;
 
     public Suggestions toEntity() {
