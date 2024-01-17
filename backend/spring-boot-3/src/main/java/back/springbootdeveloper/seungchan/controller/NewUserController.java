fa206dba15f762fd7942168113b61bd6b1fa6470
박승찬 <seungchan141414@gmail.com>
2024-01-17 21:39:45 +0900

    [Style : back] 공백 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/NewUserController.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/NewUserController.java
@@ -77,6 +77,7 @@ public class NewUserController {
         if (isNuriKing) {
             tempUserService.removeTempUserById(idOfNewUser);
         }
+        
         return BaseResponseBodyUtiil.BaseResponseBodySuccess();
     }
 }
