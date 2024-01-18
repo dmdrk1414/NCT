f5fad093ce2dd0eb69e3c213db566886e0b6bb53
박승찬 <seungchan141414@gmail.com>
2024-01-18 14:11:29 +0900

    [Test : back] 건의_게시판_작성_공백_예외_테스트

--- backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/SuggestionsControllerTest.java
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/SuggestionsControllerTest.java
@@ -143,4 +143,55 @@ class SuggestionsControllerTest {
         assertThat(stateCode).isEqualTo(CustomHttpStatus.INVALID_SELECTION_CLASSIFICATION.value());
 
     }
+
+    @ParameterizedTest
+    @ValueSource(strings =
+            {
+                    "classification",
+                    "title"
+            }
+    )
+    void 건의_게시판_작성_공백_예외_테스트(String input) throws Exception {
+        // given
+        final String url = "/suggestion/write";
+        String title = "test title";
+        String holidayPeriod = "";
+        String classification = SuggestionConstant.SUGGESTION.getClassification();
+
+        SuggestionWriteReqDto requestDto = null;
+        if (input.contains("classification")) {
+            requestDto = SuggestionWriteReqDto.builder()
+                    .title(title)
+                    .classification("")
+                    .holidayPeriod(holidayPeriod)
+                    .build();
+        }
+
+        if (input.contains("title")) {
+            requestDto = SuggestionWriteReqDto.builder()
+                    .title("")
+                    .classification(classification)
+                    .holidayPeriod(holidayPeriod)
+                    .build();
+        }
+
+        // when
+        final String requestBody = objectMapper.writeValueAsString(requestDto);
+
+        MvcResult result = mockMvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody)
+                        .header("authorization", "Bearer " + token) // token header에 담기
+                )
+                .andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+
+        // JSON 응답을 Map으로 변환
+        HttpStatus httpStatus = TestUtills.getHttpStatusFromResponse(response);
+        Integer stateCode = TestUtills.getCustomHttpStatusCodeFromResponse(response);
+
+        assertThat(httpStatus).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(stateCode).isEqualTo(CustomHttpStatus.DATA_VALID.value());
+    }
 }
