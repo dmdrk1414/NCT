57b2e4ec4855d7fb74c9cd90f2d560b24b8c698a
Ag <70638763+choeun01@users.noreply.github.com>
2024-01-21 18:35:27 +0900

    feat : suggestion 본문 창 구현

--- frontend/src/app/suggestion/[id]/page.tsx
+++ frontend/src/app/suggestion/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Header from '@/atoms/molecule/header';
+import Link from 'next/link';
 import NavigationFooter from '@/atoms/molecule/navigation-footer';
 import { useRecoilState } from 'recoil';
 import { userToken, isNuriKing } from '../../../states/index';
@@ -9,6 +10,9 @@ import { replaceRouterInitialize } from '@/utils/RouteHandling';
 import { useRouter } from 'next/navigation';
 import { axAuth } from '@/apis/axiosinstance';
 import { usePathname } from 'next/navigation';
+import Button from '../../../atoms/atom/small-button';
+import { HTTP_STATUS_OK } from '@/utils/constans/httpStatusEnum';
+import SuggestionComment from '@/atoms/molecule/suggestion-comment';
 
 interface SuggestionDataType {
   check: boolean;
@@ -17,13 +21,30 @@ interface SuggestionDataType {
   title: string;
 }
 
+interface CommentDataType {
+  name: string;
+  comment: string;
+}
+
 export default function Main() {
   const [isKing, setIsKing] = useRecoilState(isNuriKing);
   const [token, setToken] = useRecoilState(userToken);
   const router = useRouter();
   const [suggestionData, setSuggestionData] = useState<SuggestionDataType>();
+  const [userData, setUserData] = useState({ name: '' });
+  const [suggestionComment, setSuggestionComment] = useState('');
+  const [commentData, setCommentData] = useState<CommentDataType>();
+  const [commentLists, setCommentLists] = useState([]);
   const pathName: string = usePathname();
   let id: string | undefined;
+
+  const font = `text-white font-bold`;
+  const justify = `flex justify-center`;
+  const classificationCss = `w-[18%] ${justify} `;
+  const titleCss = `w-[82%] ${justify} `;
+
+  const classificationFirstCss = `${classificationCss} ${font}`;
+  const titleFirstCss = `${titleCss} ${font}`;
 
   if (typeof pathName === 'string') {
     id = pathName.split('/').pop();
@@ -50,6 +71,32 @@ export default function Main() {
     }
   };
 
+  const writeComment = () => {
+    axAuth(token)({
+      method: 'post',
+      url: '/suggestion/Comment',
+      data: {
+        name: userData,
+        content: suggestionComment,
+        holidayPeriod: '',
+      },
+    })
+      .then(response => {
+        const httpStatus = response.data.httpStatus;
+        if (httpStatus === HTTP_STATUS_OK) {
+          alert('댓글이 작성되었습니다.');
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    setSuggestionComment(value);
+  };
+
   useEffect(() => {
     // 토큰이 없을시 초기화면으로 이동
     if (hasNotToken(token)) {
@@ -62,6 +109,16 @@ export default function Main() {
   useEffect(() => {
     axAuth(token)({
       method: 'get',
+      url: '/mypage/' + id,
+    }).then(res => {
+      const data = res.data;
+      setUserData(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    axAuth(token)({
+      method: 'get',
       url: '/suggestion/' + id,
     }).then(res => {
       const data = res.data;
@@ -69,24 +126,59 @@ export default function Main() {
     });
   }, [suggestionData]);
 
+  useEffect(() => {
+    axAuth(token)({
+      method: 'get',
+      url: '/suggestion/' + id + '/comment',
+    }).then(res => {
+      const data = res.data;
+      setCommentData(data);
+    });
+  }, [commentData]);
+
   return (
     <main>
       <header>
-        <Header isVisible={false} />
+        <Link href={'/suggestion'}>
+          <p className="font-bold text-3xl ml-[1.5rem] mt-[2rem] mb-[2rem]">{suggestionData?.id}</p>
+        </Link>
       </header>
-      <article className="flex flex-col  m-[1.5rem] ">
-        <div className=" rounded w-[95%] h-[95%]">
-          <div className="flex justify-between">
-            <div>
-              <div className="">번호: {suggestionData?.id}</div>
-              <div>분류: {suggestionData?.classification}</div>
-            </div>
-            <div>
-              확인
+      <article className="flex flex-col items-center m-[1.5rem] ">
+        <div className=" w-[95%] mb-[2rem]">
+          <div className="flex justify-center">
+            <div className={`${classificationFirstCss} rounded-tl-[0.63rem] bg-blue`}>분류</div>
+            <div className={`${titleFirstCss} rounded-tr-[0.63rem] bg-blue`}>제목</div>
+          </div>
+          <div className="flex justify-center border border-blue h-[10rem] rounded-b-[0.63rem] ">
+            <div className={`${classificationCss} items-center`}>{suggestionData?.classification}</div>
+            <div className={`${titleCss} items-center`}>{suggestionData?.title}</div>
+            <div className={`${classificationCss} items-center`}>
               <input className="w-[1.1rem] mr-[0.2rem]" type="checkbox" checked={suggestionData?.check} onChange={() => pushCheck(suggestionData?.id)} id="myCheckbox" />
             </div>
           </div>
-          <div className="font-bold text-lg mt-[0.5rem]">{suggestionData?.title}</div>
+        </div>
+        <div className=" w-[95%] mb-[2rem]">
+          <div className="flex justify-center">
+            <div className={`${classificationFirstCss} rounded-tl-[0.63rem] bg-blue`}>이름</div>
+            <div className={`${titleFirstCss} rounded-tr-[0.63rem] bg-blue`}>내용</div>
+          </div>
+          <div>
+            {commentLists
+              .slice()
+              .reverse()
+              .map((item: CommentDataType, idx) => (
+                <div key={idx} className="flex justify-center border border-blue h-[2rem]">
+                  <SuggestionComment name={item.name} comment={item.comment} />
+                </div>
+              ))}
+          </div>
+          <div className="flex justify-center border border-blue h-[1.5rem] rounded-b-[0.63rem] "></div>
+        </div>
+        <div className=" w-[95%] mb-[1rem] h-[5rem]">
+          <textarea name="suggestion_content" onChange={handleChange} placeholder="댓글을 입력하세요." className={`border border-grey w-[100%] h-[100%]`} maxLength={100} />
+        </div>
+        <div className="flex mb-[2rem]" onClick={writeComment}>
+          <Button text={'댓글 쓰기'} addClass="text-xl" />
         </div>
       </article>
       <NavigationFooter isKing={isKing}></NavigationFooter>
