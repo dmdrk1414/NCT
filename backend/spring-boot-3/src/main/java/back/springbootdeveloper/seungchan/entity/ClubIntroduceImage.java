836122681c65c4f5b550d16cc671c6fd24c11a64
박승찬 <seungchan141414@gmail.com>
2024-02-15 21:50:40 +0900

    [Feat : back] ClubIntroduceImage entity 추가

---
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/entity/ClubIntroduceImage.java
@@ -0,0 +1,29 @@
+package back.springbootdeveloper.seungchan.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "club_introduce_image")
+public class ClubIntroduceImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "club_introduce_image_id")
+    private Long clubIntroduceImageId;
+
+    @Column(name = "club_introduce_image", length = 1000, nullable = false)
+    private String clubIntroduceImageUrl;
+
+    @Builder
+    public ClubIntroduceImage(String clubIntroduceImageUrl) {
+        this.clubIntroduceImageUrl = clubIntroduceImageUrl;
+    }
+
+    public void updateClubIntroduceImageUrl(String clubIntroduceImageUrl) {
+        this.clubIntroduceImageUrl = clubIntroduceImageUrl;
+    }
+}
