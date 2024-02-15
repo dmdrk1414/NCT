3359024740502d2b3f1c5ec44a27b943fe484f3d
박승찬 <seungchan141414@gmail.com>
2024-02-15 22:12:23 +0900

    [Feat : back] VacationTokenControl entity 추가

---
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/entity/VacationTokenControl.java
@@ -0,0 +1,29 @@
+package back.springbootdeveloper.seungchan.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "vacation_token_control")
+public class VacationTokenControl {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vacation_token_control_id")
+    private Long vacationTokenControlId;
+
+    @Column(name = "like_count")
+    private Integer vacationTokenControl = 5;
+
+    @Builder
+    public VacationTokenControl(Integer vacationTokenControl) {
+        this.vacationTokenControl = vacationTokenControl;
+    }
+
+    public void updateVacationTokenControl(Integer vacationTokenControl) {
+        this.vacationTokenControl = vacationTokenControl;
+    }
+}
