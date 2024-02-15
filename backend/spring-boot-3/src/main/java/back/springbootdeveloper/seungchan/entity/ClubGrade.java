4fae7e87e2f2b3fd188fd4e26e7714e5e3486ef7
박승찬 <seungchan141414@gmail.com>
2024-02-15 21:38:02 +0900

    [Feat : back] ClubGrade entity에 컬럼 추가

---
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/entity/ClubGrade.java
@@ -0,0 +1,34 @@
+package back.springbootdeveloper.seungchan.entity;
+
+import back.springbootdeveloper.seungchan.constant.entity.CLUB_ARTICLE_SUGGESTION_CHECK;
+import back.springbootdeveloper.seungchan.constant.entity.CLUB_GRADE;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import jakarta.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "club_grade")
+public class ClubGrade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "club_grade_id")
+    private Long clubGradeId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "club_grade", length = 15)
+    private CLUB_GRADE clubGrade;
+}
