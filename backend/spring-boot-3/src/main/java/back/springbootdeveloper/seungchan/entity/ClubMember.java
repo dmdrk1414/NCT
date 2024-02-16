abbeb59b56fdbadb64d27fcac37c8d11b99b07f5
박승찬 <seungchan141414@gmail.com>
2024-02-17 03:11:31 +0900

    [Feat : back]  ClubMember 참조 null 불가 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/entity/ClubMember.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/entity/ClubMember.java
@@ -1,5 +1,6 @@
 package back.springbootdeveloper.seungchan.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.*;
@@ -11,21 +12,30 @@ import jakarta.persistence.*;
 public class ClubMember {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "club_member_id")
+    @Column(name = "club_member_id", nullable = false)
     private Long clubMemberId;
 
-    @Column(name = "member_id")
-    private Long memberId;
+    @Column(name = "member_id", nullable = false)
+    private Long memberId; // member
 
-    @Column(name = "club_id")
-    private Long clubId;
+    @Column(name = "club_id", nullable = false)
+    private Long clubId; // club
 
-    @Column(name = "club_grade_id")
-    private Long clubGradeId;
+    @Column(name = "club_grade_id", nullable = false)
+    private Long clubGradeId; // member - club
 
-    @Column(name = "attendance_state_id")
-    private Long attendanceSateId;
+    @Column(name = "attendance_state_id", nullable = false)
+    private Long attendanceSateId; // member - club
 
-    @Column(name = "club_member_information_id")
-    private Long clubMemberInformationId;
+    @Column(name = "club_member_information_id", nullable = false)
+    private Long clubMemberInformationId; // member - club
+
+    @Builder
+    public ClubMember(Long memberId, Long clubId, Long clubGradeId, Long attendanceSateId, Long clubMemberInformationId) {
+        this.memberId = memberId;
+        this.clubId = clubId;
+        this.clubGradeId = clubGradeId;
+        this.attendanceSateId = attendanceSateId;
+        this.clubMemberInformationId = clubMemberInformationId;
+    }
 }
