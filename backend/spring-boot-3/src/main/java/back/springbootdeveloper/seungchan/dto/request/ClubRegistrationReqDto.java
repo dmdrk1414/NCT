12bf153c37225879fe67b4ff1117131648fa71dd
begin2ee <108245208+begin2ee@users.noreply.github.com>
2024-02-16 20:33:14 +0900

    [Feat : back] 팀 등록을 위한  Requst Dto 추가

---
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/request/ClubRegistrationReqDto.java
@@ -0,0 +1,8 @@
+package back.springbootdeveloper.seungchan.dto.request;
+
+public class ClubRegistrationReqDto {
+    private String clubName;
+    private String clubIntroduction;
+    private String clubProfileImage;
+    private String clubInformationImages;
+}
