28ab4a95c71657509bd95b598643e829d91ec228
박승찬 <seungchan141414@gmail.com>
2024-02-17 22:24:47 +0900

    [Feat : back] Club - Member 관계 매핑을 위한 메서드 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/EntityUtilService.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/EntityUtilService.java
@@ -1,6 +1,9 @@
 package back.springbootdeveloper.seungchan.service;
 
+import back.springbootdeveloper.seungchan.constant.entity.CLUB_GRADE;
 import back.springbootdeveloper.seungchan.entity.*;
+import back.springbootdeveloper.seungchan.repository.AttendanceSateRepository;
+import back.springbootdeveloper.seungchan.repository.ClubMemberRepository;
 import back.springbootdeveloper.seungchan.repository.ClubRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +15,14 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class EntityUtilService {
     private final ClubRepository clubRepository;
+    private final AttendanceSateRepository attendanceSateRepository;
+    private final ClubMemberRepository clubMemberRepository;
 
     @Autowired
-    public EntityUtilService(ClubRepository clubRepository) {
+    public EntityUtilService(ClubRepository clubRepository, AttendanceSateRepository attendanceSateRepository, ClubMemberRepository clubMemberRepository) {
         this.clubRepository = clubRepository;
+        this.attendanceSateRepository = attendanceSateRepository;
+        this.clubMemberRepository = clubMemberRepository;
     }
 
     private static Club createClub(String clubName, String clubIntroduce, String clubProfileImage) {
@@ -26,10 +33,21 @@ public class EntityUtilService {
                 .build();
     }
 
+    private static ClubMember creatClubMember(Member member, Club club, ClubMemberInformation clubMemberInformation, AttendanceSate attendanceSate, ClubGrade clubGrade) {
+        return ClubMember.builder()
+                .memberId(member.getMemberId())
+                .clubMemberInformationId(clubMemberInformation.getClubMemberInformationId())
+                .clubId(club.getClubId())
+                .clubGradeId(clubGrade.getClubGradeId())
+                .attendanceSateId(attendanceSate.getAttendanceStateId())
+                .clubMemberInformationId(clubMemberInformation.getClubMemberInformationId())
+                .build();
+    }
+
     /**
      * Club을 생성하고 저장합니다.
      *
-     * @param clubName            Club의 이름
+     * @param clubName            Club 이름
      * @param clubIntroduce       Club 자기소개
      * @param clubProfileImage    Club 프로필 사진 URL
      * @param clubIntroduceImages Club 자기소개 사진 리스트
@@ -51,9 +69,51 @@ public class EntityUtilService {
     }
 
     /**
+     * ClubMember 생성하고 저장합니다.
+     *
+     * @param member                ClubMember의 회원 정보
+     * @param club                  ClubMember
+     * @param CLUB_GRADE            ClubMember의 등급
+     * @param clubMemberInformation ClubMember의 추가 정보
+     * @return 생성된 클럽 멤버
+     */
+    @Transactional
+    public ClubMember applyClub(Member member, Club club, CLUB_GRADE CLUB_GRADE, ClubMemberInformation clubMemberInformation) {
+        AttendanceSate attendanceSate = attendanceSateRepository.save(createAttendanceState());
+        ClubGrade clubGrade = new ClubGrade(CLUB_GRADE);
+
+        // ClubMember
+        //   - Member, Club, ClubMemberInformation, AttendanceSate, ClubGrade
+        ClubMember clubMember = creatClubMember(member, club, clubMemberInformation, attendanceSate, clubGrade);
+
+        return clubMemberRepository.save(clubMember);
+    }
+
+    /**
+     * AttendanceState 생성
+     *
+     * @return AttendanceState 반환
+     */
+    private AttendanceSate createAttendanceState() {
+        AttendanceSate attendanceSate = new AttendanceSate();
+        AttendanceCheckTime attendanceCheckTime = new AttendanceCheckTime();
+        AttendanceWeekDate attendanceWeekDate = new AttendanceWeekDate();
+        VacationToken vacationToken = new VacationToken();
+
+        // attendanceSate - attendanceCheckTime
+        attendanceSate.setAttendanceCheckTime(attendanceCheckTime);
+        // attendanceSate - attendanceWeekDate
+        attendanceSate.setAttendanceWeekDate(attendanceWeekDate);
+        // attendanceSate - vacationToken
+        attendanceSate.setVacationToken(vacationToken);
+
+        return attendanceSate;
+    }
+
+    /**
      * ClubControl 생성
      *
-     * @return
+     * @return ClubControl 반환
      */
     private ClubControl createClubControl() {
         ClubControl clubControl = new ClubControl();
