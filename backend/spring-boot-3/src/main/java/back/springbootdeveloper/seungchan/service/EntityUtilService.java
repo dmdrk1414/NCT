d9f410bb9c21030d2b2fc8fd1b71691b2475917f
박승찬 <seungchan141414@gmail.com>
2024-02-17 21:59:54 +0900

    [Feat : back] ClubControl - Club 매핑 및 Club 등록, 생성 메서드 수정

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/EntityUtilService.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/EntityUtilService.java
@@ -1,8 +1,6 @@
 package back.springbootdeveloper.seungchan.service;
 
-import back.springbootdeveloper.seungchan.entity.AttendanceNumber;
-import back.springbootdeveloper.seungchan.entity.Club;
-import back.springbootdeveloper.seungchan.entity.ClubIntroduceImage;
+import back.springbootdeveloper.seungchan.entity.*;
 import back.springbootdeveloper.seungchan.repository.ClubRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +18,14 @@ public class EntityUtilService {
         this.clubRepository = clubRepository;
     }
 
+    private static Club createClub(String clubName, String clubIntroduce, String clubProfileImage) {
+        return Club.builder()
+                .clubName(clubName)
+                .clubIntroduce(clubIntroduce)
+                .clubProfileImage(clubProfileImage)
+                .build();
+    }
+
     /**
      * Club을 생성하고 저장합니다.
      *
@@ -30,17 +36,35 @@ public class EntityUtilService {
      * @return 저장된 Club
      */
     @Transactional
-    public Club applyClub(String clubName, String clubIntroduce, String clubProfileImage, List<ClubIntroduceImage> clubIntroduceImages) {
-        final Club club = Club.builder()
-                .clubName(clubName)
-                .clubIntroduce(clubIntroduce)
-                .clubProfileImage(clubProfileImage)
-                .build();
+    public Club saveClub(String clubName, String clubIntroduce, String clubProfileImage, List<ClubIntroduceImage> clubIntroduceImages) {
+        final Club club = createClub(clubName, clubIntroduce, clubProfileImage);
+        final ClubControl clubControl = createClubControl();
 
+        // Club - AttendanceNumber
         club.addAttendanceNumber(new AttendanceNumber());
-        clubIntroduceImages.forEach(clubIntroduceImage
-                -> club.addClubIntroduceImage(clubIntroduceImage));
+        // Club - ClubIntroduceImages
+        clubIntroduceImages.forEach(clubIntroduceImage -> club.addClubIntroduceImage(clubIntroduceImage));
+        // Club - ClubControl
+        club.setClubControl(clubControl);
 
         return clubRepository.save(club);
+    }
+
+    /**
+     * ClubControl 생성
+     *
+     * @return
+     */
+    private ClubControl createClubControl() {
+        ClubControl clubControl = new ClubControl();
+        VacationTokenControl vacationTokenControl = new VacationTokenControl();
+        AttendanceWeek attendanceWeek = new AttendanceWeek();
+
+        // ClubControl - VacationTokenControl
+        clubControl.setVacationTokenControl(vacationTokenControl);
+        // ClubControl - AttendanceWeek
+        clubControl.setAttendanceWeek(attendanceWeek);
+
+        return clubControl;
     }
 }
