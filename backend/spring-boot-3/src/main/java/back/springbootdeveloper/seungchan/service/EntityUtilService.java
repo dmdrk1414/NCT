9ff78457eb0cd0a39558275c927699ac2e169be3
박승찬 <seungchan141414@gmail.com>
2024-02-17 21:44:21 +0900

    [Feat : back] VacationTokenControl 필드 변경

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/EntityUtilService.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/EntityUtilService.java
@@ -13,5 +13,34 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 public class EntityUtilService {
+    private final ClubRepository clubRepository;
 
+    @Autowired
+    public EntityUtilService(ClubRepository clubRepository) {
+        this.clubRepository = clubRepository;
+    }
+
+    /**
+     * Club을 생성하고 저장합니다.
+     *
+     * @param clubName            Club의 이름
+     * @param clubIntroduce       Club 자기소개
+     * @param clubProfileImage    Club 프로필 사진 URL
+     * @param clubIntroduceImages Club 자기소개 사진 리스트
+     * @return 저장된 Club
+     */
+    @Transactional
+    public Club applyClub(String clubName, String clubIntroduce, String clubProfileImage, List<ClubIntroduceImage> clubIntroduceImages) {
+        final Club club = Club.builder()
+                .clubName(clubName)
+                .clubIntroduce(clubIntroduce)
+                .clubProfileImage(clubProfileImage)
+                .build();
+
+        club.addAttendanceNumber(new AttendanceNumber());
+        clubIntroduceImages.forEach(clubIntroduceImage
+                -> club.addClubIntroduceImage(clubIntroduceImage));
+
+        return clubRepository.save(club);
+    }
 }
--- backend/spring-boot-3/src/main/resources/data.sql
+++ backend/spring-boot-3/src/main/resources/data.sql
@@ -1254,15 +1254,15 @@ VALUES (5, '2024-02-17 17:06:46.349665', '2024-02-17 17:06:46.349648', 'POSSIBLE
         'POSSIBLE', 'POSSIBLE', 'POSSIBLE');
 
 -- vacation_token_control
-INSERT INTO vacation_token_control (vacation_token_control_id, create_date, update_date, like_count)
+INSERT INTO vacation_token_control (vacation_token_control_id, create_date, update_date, vacation_token_control)
 VALUES (1, '2024-02-17 17:06:45.156650', '2024-02-17 17:06:45.155774', 5);
-INSERT INTO vacation_token_control (vacation_token_control_id, create_date, update_date, like_count)
+INSERT INTO vacation_token_control (vacation_token_control_id, create_date, update_date, vacation_token_control)
 VALUES (2, '2024-02-17 17:06:45.745656', '2024-02-17 17:06:45.745642', 5);
-INSERT INTO vacation_token_control (vacation_token_control_id, create_date, update_date, like_count)
+INSERT INTO vacation_token_control (vacation_token_control_id, create_date, update_date, vacation_token_control)
 VALUES (3, '2024-02-17 17:06:45.973500', '2024-02-17 17:06:45.973486', 5);
-INSERT INTO vacation_token_control (vacation_token_control_id, create_date, update_date, like_count)
+INSERT INTO vacation_token_control (vacation_token_control_id, create_date, update_date, vacation_token_control)
 VALUES (4, '2024-02-17 17:06:46.255580', '2024-02-17 17:06:46.255569', 5);
-INSERT INTO vacation_token_control (vacation_token_control_id, create_date, update_date, like_count)
+INSERT INTO vacation_token_control (vacation_token_control_id, create_date, update_date, vacation_token_control)
 VALUES (5, '2024-02-17 17:06:46.349786', '2024-02-17 17:06:46.349775', 5);
 
 -- club_control
