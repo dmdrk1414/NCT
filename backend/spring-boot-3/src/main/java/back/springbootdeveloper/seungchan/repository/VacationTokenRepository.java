2d0313e10a238ed5c29e2a932d7b3a2bdb51d574
박승찬 <seungchan141414@gmail.com>
2024-02-24 19:57:18 +0900

    [Feat : back] VacationTokenRepository 생성

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/repository/VacationTokenRepository.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/repository/VacationTokenRepository.java
@@ -3,8 +3,9 @@ package back.springbootdeveloper.seungchan.repository;
 import back.springbootdeveloper.seungchan.entity.VacationToken;
 import back.springbootdeveloper.seungchan.entity.VacationTokenControl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@org.springframework.stereotype.Repository
+@Repository
 public interface VacationTokenRepository extends JpaRepository<VacationToken, Long> {
 
 }
