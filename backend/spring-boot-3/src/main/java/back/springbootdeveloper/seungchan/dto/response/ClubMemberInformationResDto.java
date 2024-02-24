7376df08f3e9e9f7332949db6d3c465d1f18a109
박승찬 <seungchan141414@gmail.com>
2024-02-24 12:53:11 +0900

    [Feat : back] Club Detail Page 회원 상세 조회 기능 완성

---
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/response/ClubMemberInformationResDto.java
@@ -0,0 +1,17 @@
+package back.springbootdeveloper.seungchan.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class ClubMemberInformationResDto {
+    private String name;
+    private String major;
+    private String studentId;
+    private String selfIntroduction;
+}
