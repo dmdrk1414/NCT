424a6aab594ce2659e34223924cd90221ce5e386
박승찬 <seungchan141414@gmail.com>
2024-02-25 03:49:34 +0900

    [Feat : back] 클럽 멤버의 오늘의 출석 주차 출석상태를 업데이트메서드 추가
    금일에 해당하는 출석 상태를 업데이트를 한다.
    만약 VACATION, ABSENCE, ATTENDANCE을 매개변수로 주면
    해당하는 요일의 출석상태를 원하는 형식으로 업데이트 가능

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/AttendanceWeekDateService.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/AttendanceWeekDateService.java
@@ -16,5 +16,47 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AttendanceWeekDateService {
+    private final ClubMemberRepository clubMemberRepository;
+    private final AttendanceStateRepository attendanceStateRepository;
+    private final AttendanceWeekDateRepository attendanceWeekDateRepository;
 
+    /**
+     * 클럽 멤버의 오늘의 출석 주차 출석상태를 업데이트합니다.
+     *
+     * @param clubMemberId   출석 주차 날짜를 업데이트할 클럽 멤버의 ID
+     * @param attendanceEnum 업데이트할 출석 상태
+     * @throws EntityNotFoundException 지정된 ID에 해당하는 클럽 멤버나 출석 상태가 없을 때 발생하는 예외
+     */
+    @Transactional
+    public void updateTodayAttendanceWeekDate(Long clubMemberId, ATTENDANCE_STATE attendanceEnum) {
+
+        ClubMember clubMember = clubMemberRepository.findById(clubMemberId).orElseThrow(EntityNotFoundException::new);
+        AttendanceState attendanceState = attendanceStateRepository.findById(clubMember.getAttendanceStateId()).orElseThrow(EntityNotFoundException::new);
+        AttendanceWeekDate attendanceWeekDate = attendanceState.getAttendanceWeekDate();
+
+        // 원하는 출석 상태에 의해 변경
+        updateAttendanceStateWithAttendanceEnum(attendanceEnum, attendanceWeekDate);
+
+        // 업데이트 적용
+        attendanceWeekDateRepository.save(attendanceWeekDate);
+
+    }
+
+    /**
+     * 주어진 출석 상태에 따라 출석 주차 날짜를 업데이트합니다.
+     *
+     * @param attendanceEnum     업데이트할 출석 상태
+     * @param attendanceWeekDate 출석 주차 날짜 객체
+     */
+    private void updateAttendanceStateWithAttendanceEnum(ATTENDANCE_STATE attendanceEnum, AttendanceWeekDate attendanceWeekDate) {
+        if (attendanceEnum == ATTENDANCE_STATE.VACATION) {
+            attendanceWeekDate.updateVacationAtToday();
+        }
+        if (attendanceEnum == ATTENDANCE_STATE.ABSENCE) {
+            attendanceWeekDate.updateAbsenceAtToday();
+        }
+        if (attendanceEnum == ATTENDANCE_STATE.ATTENDANCE) {
+            attendanceWeekDate.updateAttendanceAtToday();
+        }
+    }
 }
