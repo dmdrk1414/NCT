038b19674c39fb113959e18eb8db07e8c946fc65
박승찬 <seungchan141414@gmail.com>
2024-02-25 16:56:19 +0900

    [Feat : back] ClubArticle repository 생성

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/repository/ClubArticleRepository.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/repository/ClubArticleRepository.java
@@ -6,6 +6,9 @@ import back.springbootdeveloper.seungchan.entity.ClubGrade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClubArticleRepository extends JpaRepository<ClubArticle, Long> {
+    List<ClubArticle> findAllByClubMemberId(Long clubMemberId);
 }
