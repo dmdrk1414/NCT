1dcbfcc02154a73817c8ae906eb9911da304f93c
박승찬 <seungchan141414@gmail.com>
2024-02-26 00:02:41 +0900

    [Feat : back] data sql문 추가

--- backend/spring-boot-3/src/main/resources/sql/club_grade.sql
+++
@@ -1,9 +0,0 @@
--- club_grade
-INSERT INTO club_grade (club_grade_id, create_date, update_date, club_grade)
-VALUES (1, '2024-02-17 02:17:30.559515', '2024-02-17 02:17:30.558625', 'LEADER');
-INSERT INTO club_grade (club_grade_id, create_date, update_date, club_grade)
-VALUES (2, '2024-02-17 02:17:30.561180', '2024-02-17 02:17:30.561101', 'DEPUTY_LEADER');
-INSERT INTO club_grade (club_grade_id, create_date, update_date, club_grade)
-VALUES (3, '2024-02-17 02:17:30.562760', '2024-02-17 02:17:30.562597', 'MEMBER');
-INSERT INTO club_grade (club_grade_id, create_date, update_date, club_grade)
-VALUES (4, '2024-02-17 02:17:30.564334', '2024-02-17 02:17:30.564236', 'DORMANT');
