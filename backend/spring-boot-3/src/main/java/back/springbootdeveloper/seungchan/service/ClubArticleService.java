5998005e3804fab2e48273462dbc43f7e03e17fc
박승찬 <seungchan141414@gmail.com>
2024-02-26 18:54:20 +0900

    [Style : back] 주석 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/ClubArticleService.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/ClubArticleService.java
@@ -16,6 +16,13 @@ import java.util.List;
 public class ClubArticleService {
     private final ClubArticleRepository clubArticleRepository;
 
+    /**
+     * 주어진 clubMemberId에 해당하는 클럽 회원의 게시글을 가져와서 해당 회원이 작성한 게시글을 MyClubArticle 객체의 리스트로 변환한 후,
+     * ClubMemberArticlesResDto 객체에 설정하여 반환합니다.
+     *
+     * @param clubMemberId 클럽 회원 ID
+     * @return ClubMemberArticlesResDto 객체
+     */
     public ClubMemberArticlesResDto getClubMemberArticlesResDto(Long clubMemberId) {
         List<MyClubArticle> myClubArticles = new ArrayList<>();
         List<ClubArticle> clubArticles = clubArticleRepository.findAllByClubMemberId(clubMemberId);
