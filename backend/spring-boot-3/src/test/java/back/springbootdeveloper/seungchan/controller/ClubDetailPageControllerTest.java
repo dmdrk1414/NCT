16f256a10819db6d8e0959ae24b23983a2205c8f
박승찬 <seungchan141414@gmail.com>
2024-02-29 13:41:21 +0900

    [Test : back] 동아리_소개_페이지_회원_추방_예외_실장_로드인_검증_테스트

--- backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/ClubDetailPageControllerTest.java
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/ClubDetailPageControllerTest.java
@@ -154,7 +154,7 @@ class ClubDetailPageControllerTest {
     @Test
     void 동아리_소개_페이지_휴가_제공_예외_일반회원_검증_테스트() throws Exception {
         // given
-        // 실장이 아닌 유저 로그인
+        // 대표이 아닌 유저 로그인
         final String token = testCreateUtil.create_token_one_club_deputy_leader_member();
         final String url = "/clubs/informations/{club_id}/details/{club_member_id}/vacation";
 
@@ -187,7 +187,7 @@ class ClubDetailPageControllerTest {
     @Test
     void 동아리_소개_페이지_회원_추방_테스트() throws Exception {
         // given
-        // 실장 유저 로그인
+        // 대표 유저 로그인
         final String token = testCreateUtil.create_token_one_club_leader_member();
         final String url = "/clubs/informations/{club_id}/details/{club_member_id}/expulsion";
 
@@ -216,9 +216,36 @@ class ClubDetailPageControllerTest {
     }
 
     @Test
-    void 동아리_소개_페이지_회원_추방_예외_대상_실장_검증_테스트() throws Exception {
+    void 동아리_소개_페이지_회원_추방_예외_실장_로드인_검증_테스트() throws Exception {
         // given
-        // 실장 유저 로그인
+        // 대표가 아닌 유저 로그인
+        final String token = testCreateUtil.create_token_one_club_deputy_leader_member();
+        final String url = "/clubs/informations/{club_id}/details/{club_member_id}/expulsion";
+
+        // 타겟 유저
+        final Member targetMember = testCreateUtil.get_entity_one_club_leader_member();
+        final ClubMember targetClubMember = clubMemberRepository.findByClubIdAndMemberId(targetClubOneId, targetMember.getMemberId()).get();
+
+        // when
+        ResultActions result = mockMvc.perform(post(url, targetClubOneId, targetClubMember.getClubMemberId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header("authorization", "Bearer " + token) // token header에 담기
+        );
+
+        // then
+
+        result
+                .andExpect(jsonPath("$.message").value(ResponseMessage.BAD_NOT_LEADER_CLUB.get()))
+                .andExpect(jsonPath("$.httpStatus").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.statusCode").value(HttpStatus.BAD_REQUEST.value()));
+
+    }
+
+    @Test
+    void 동아리_소개_페이지_회원_추방_예외_대상_대표_검증_테스트() throws Exception {
+        // given
+        // 대표 유저 로그인
         final String token = testCreateUtil.create_token_one_club_leader_member();
         final String url = "/clubs/informations/{club_id}/details/{club_member_id}/expulsion";
 
