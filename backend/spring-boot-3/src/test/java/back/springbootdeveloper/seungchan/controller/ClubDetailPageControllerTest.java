7b05fc2549cd5a397700a7b676b15e6ac38c8964
박승찬 <seungchan141414@gmail.com>
2024-02-29 14:16:05 +0900

    [Test : back] 동아리_소개_페이지_회원_휴면_전환_예외_대상_대표_테스트

--- backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/ClubDetailPageControllerTest.java
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/ClubDetailPageControllerTest.java
@@ -355,4 +355,31 @@ class ClubDetailPageControllerTest {
                 .andExpect(jsonPath("$.statusCode").value(HttpStatus.BAD_REQUEST.value()));
 
     }
+
+    @Test
+    void 동아리_소개_페이지_회원_휴면_전환_예외_대상_대표_테스트() throws Exception {
+        // given
+        // 유저 로그인
+        final String token = testCreateUtil.create_token_one_club_leader_member();
+        final String url = "/clubs/informations/{club_id}/details/{club_member_id}/dormancy";
+
+        // 타겟 유저
+        final Member targetMember = testCreateUtil.get_entity_one_club_leader_member();
+        final ClubMember targetClubMember = clubMemberRepository.findByClubIdAndMemberId(targetClubOneId, targetMember.getMemberId()).get();
+
+        // when
+        ResultActions result = mockMvc.perform(post(url, targetClubOneId, targetClubMember.getClubMemberId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header("authorization", "Bearer " + token) // token header에 담기
+        );
+
+        // then
+
+        result
+                .andExpect(jsonPath("$.message").value(ResponseMessage.BAD_TARGET_LEADER_MEMBER.get()))
+                .andExpect(jsonPath("$.httpStatus").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.statusCode").value(HttpStatus.BAD_REQUEST.value()));
+
+    }
 }
