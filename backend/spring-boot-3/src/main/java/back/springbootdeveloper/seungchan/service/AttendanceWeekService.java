38bb4351395372c25f6f24bccf45ffa9b7cd54ed
박승찬 <seungchan141414@gmail.com>
2024-02-29 20:56:55 +0900

    [Feat : back] 클럽 출석 체크 오늘 날짜 출석 가능 기능 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/AttendanceWeekService.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/service/AttendanceWeekService.java
@@ -17,5 +17,21 @@ import java.time.DayOfWeek;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AttendanceWeekService {
+    private final ClubRepository clubRepository;
 
+    /**
+     * 특정 클럽에서 오늘의 가능한 상태를 가져옵니다.
+     *
+     * @param clubId         클럽 식별자
+     * @param todayDayOfWeek 오늘의 요일
+     * @return 오늘의 가능한 상태
+     * @throws EntityNotFoundException 만약 주어진 clubId에 해당하는 클럽이 존재하지 않을 경우 발생합니다.
+     */
+    public String getTodayPossibleStatus(Long clubId, DayOfWeek todayDayOfWeek) {
+        Club club = clubRepository.findById(clubId).orElseThrow(EntityNotFoundException::new);
+        ClubControl clubControl = club.getClubControl();
+        AttendanceWeek attendanceWeek = clubControl.getAttendanceWeek();
+
+        return attendanceWeek.getStatusForDay(todayDayOfWeek);
+    }
 }
