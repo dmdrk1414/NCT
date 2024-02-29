c5bb08632d7f57546efa1ab33218241bed570bcd
박승찬 <seungchan141414@gmail.com>
2024-02-29 15:00:28 +0900

    [Feat : back] 금일 DayOfWeek enum 가져오는 기능 추가

---
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/util/DayUtil.java
@@ -0,0 +1,26 @@
+package back.springbootdeveloper.seungchan.util;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Year;
+
+public class DayUtil {
+
+    /**
+     * 현재 날짜에 해당하는 요일을 반환합니다.
+     *
+     * @return 현재 날짜에 해당하는 요일
+     */
+    public static DayOfWeek getTodayDayOfWeek() {
+        // 현재 날짜를 가져옵니다.
+        LocalDate today = LocalDate.now();
+
+        // 현재 날짜에 해당하는 요일을 가져옵니다.
+        return today.getDayOfWeek();
+    }
+
+    public static String getYear() {
+        Year currentYear = Year.now();
+        return String.valueOf(currentYear.getValue());
+    }
+}
