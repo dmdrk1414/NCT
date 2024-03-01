ead182f2873159ce415f7b9aa3f0df960681611e
박승찬 <seungchan141414@gmail.com>
2024-03-01 22:46:31 +0900

    [Refator : back] 변수 위치 변경

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/response/ClubMemberDetailResDto.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/dto/response/ClubMemberDetailResDto.java
@@ -13,8 +13,8 @@ import java.util.List;
 @Builder
 public class ClubMemberDetailResDto {
     private String clubName;
-    private ClubMemberAttendanceCheckDate clubMemberAttendanceCheckDate;
     private Long myClubMemberId;
     private String myClubGrade;
+    private ClubMemberAttendanceCheckDate clubMemberAttendanceCheckDate;
     private List<ClubMemberResponse> clubMembers;
 }
