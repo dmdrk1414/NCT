18c684c6ef2a3bc51597f8e1a67a82fa4e22b0a0
박승찬 <seungchan141414@gmail.com>
2024-03-02 00:26:52 +0900

    [Test : back] Mypage Controller 테스트 클래스 추가

---
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/MyPageControllerTest.java
@@ -0,0 +1,32 @@
+package back.springbootdeveloper.seungchan.controller;
+
+import back.springbootdeveloper.seungchan.annotation.MoariumSpringBootTest;
+import back.springbootdeveloper.seungchan.entity.Member;
+import back.springbootdeveloper.seungchan.testutil.TestCreateUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MoariumSpringBootTest
+class MyPageControllerTest {
+
+  @Autowired
+  protected ObjectMapper objectMapper;
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private TestCreateUtil testCreateUtil;
+  private Member memberOneClubLeader;
+  private Long targetClubOneId;
+  private String token;
+
+  @BeforeEach
+  void setUp() {
+    memberOneClubLeader = testCreateUtil.get_entity_one_club_leader_member();
+    targetClubOneId = testCreateUtil.getONE_CLUB_ID();
+
+  }
+}
