ca3c99b53d0538bb7b069d9cc7b30ee4bad95a75
박승찬 <seungchan141414@gmail.com>
2024-03-02 18:58:05 +0900

    [Test : back] C팀_게시판_수정_API_테스트

--- backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/ClubArticleControllerTest.java
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/ClubArticleControllerTest.java
@@ -1,14 +1,30 @@
 package back.springbootdeveloper.seungchan.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import back.springbootdeveloper.seungchan.annotation.MoariumSpringBootTest;
+import back.springbootdeveloper.seungchan.constant.dto.response.ResponseMessage;
+import back.springbootdeveloper.seungchan.dto.request.UpdateClubArticlePutDto;
+import back.springbootdeveloper.seungchan.entity.Club;
+import back.springbootdeveloper.seungchan.entity.ClubArticle;
+import back.springbootdeveloper.seungchan.entity.ClubMember;
 import back.springbootdeveloper.seungchan.entity.Member;
+import back.springbootdeveloper.seungchan.repository.ClubArticleRepository;
+import back.springbootdeveloper.seungchan.repository.ClubMemberRepository;
+import back.springbootdeveloper.seungchan.repository.ClubRepository;
+import back.springbootdeveloper.seungchan.service.ClubArticleService;
 import back.springbootdeveloper.seungchan.testutil.TestCreateUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @MoariumSpringBootTest
 class ClubArticleControllerTest {
@@ -19,6 +35,14 @@ class ClubArticleControllerTest {
   private MockMvc mockMvc;
   @Autowired
   private TestCreateUtil testCreateUtil;
+  @Autowired
+  private ClubRepository clubRepository;
+  @Autowired
+  private ClubArticleRepository clubArticleRepository;
+  @Autowired
+  private ClubMemberRepository clubMemberRepository;
+  @Autowired
+  private ClubArticleService clubArticleService;
   private Member memberOneClubLeader;
   private Long targetClubOneId;
   private String token;
@@ -29,5 +53,49 @@ class ClubArticleControllerTest {
     targetClubOneId = testCreateUtil.getONE_CLUB_ID();
   }
 
-  
+  @Test
+  void 팀_게시판_수정_API_테스트() throws Exception {
+    // given
+    // 유저 로그인
+    final String token = testCreateUtil.create_token_one_club_leader_member();
+    final String url = "/clubs/informations/{club_id}/articles/{article_id}";
+
+    // 검증을 위한 데이터 준비
+    final Club targetClub = clubRepository.findById(targetClubOneId).get();
+    final Member targetMember = memberOneClubLeader;
+    final ClubMember targetClubMember = clubMemberRepository.findByClubIdAndMemberId(
+        targetClub.getClubId(), targetMember.getMemberId()).get();
+    final ClubArticle targetClubArticle = clubArticleService.findFirstByClubArticleId(
+        targetClubMember.getClubMemberId());
+
+    final String updateTitle = "테스트를 위한 업데이트 클럽게시물 제목";
+    final String updateContent = "테스트를 위한 업데이트 클럽게시물 내용";
+    final UpdateClubArticlePutDto requestDto = UpdateClubArticlePutDto.builder()
+        .clubArticleUpdateTitle(updateTitle)
+        .clubArticleUpdateContent(updateContent)
+        .build();
+
+    // when
+    final String requestBody = objectMapper.writeValueAsString(requestDto);
+
+    ResultActions result = mockMvc.perform(
+        put(url, targetClub.getClubId(), targetClubArticle.getClubArticleId())
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(requestBody)
+            .header("authorization", "Bearer " + token) // token header에 담기
+    );
+
+    final ClubArticle resultClubArticle = clubArticleRepository.findById(
+        targetClubArticle.getClubArticleId()).get();
+
+    // than
+    result
+        .andExpect(jsonPath("$.message").value(ResponseMessage.UPDATE_CLUB_ARTICLE.get()))
+        .andExpect(jsonPath("$.httpStatus").value(HttpStatus.OK.getReasonPhrase()))
+        .andExpect(jsonPath("$.statusCode").value(HttpStatus.OK.value()));
+
+    assertThat(resultClubArticle.getTitle()).isEqualTo(updateTitle);
+    assertThat(resultClubArticle.getContent()).isEqualTo(updateContent);
+  }
 }
