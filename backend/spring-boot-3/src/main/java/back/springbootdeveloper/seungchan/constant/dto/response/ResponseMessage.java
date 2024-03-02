6dcb751a2ddc9fa6c5ea6832d764855fac97ec23
박승찬 <seungchan141414@gmail.com>
2024-03-02 21:37:20 +0900

    [Feat : back] 팀 건의 게시판 - 상세 페이지 - 답변 쓰기 기능 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/constant/dto/response/ResponseMessage.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/constant/dto/response/ResponseMessage.java
@@ -20,13 +20,15 @@ public enum ResponseMessage {
   BAD_ALREADY_REPORT_CLUB_ARTICLE("신고에 실패하였습니다."),
   SUCCESS_REPORT_CLUB_ARTICLE("신고 접수 되었습니다.."),
   SUCCESS_SUGGESTION_ANSWER("답변이 완료 되었습니다."),
-  BAD_SUGGESTION_ANSWER("답변이 실패 되었습니다."),
+  BAD_SUGGESTION_ANSWER("답변이 실패 되었습니다. 대표 부대표만 가능합니다."),
   BAD_ALREADY_TODAY_UPDATE_ATTENDANCE_STATE("이미 결석, 휴가를 하였 습니다."),
   BAD_DORMANT_TODAY_ATTENDANCE_STATE("휴면 회원은 출석을 할 수 없습니다."),
   BAD_NOT_LEADER_CLUB("클럽의 대표가 아닙니다."),
   BAD_TARGET_LEADER_MEMBER("대표을 대상으로 할수 없는 기능입니다."),
   BAD_REQUEST_NOT_CLUB_CHECK_STATE("클럽 출석 체크 지정일이 아닙니다."),
-  BAD_NOT_SAME_LOGIN_TARGET_MEMBER("타인의 계정에 시도 할 수 없는 기능 입니다.");
+  BAD_NOT_SAME_LOGIN_TARGET_MEMBER("타인의 계정에 시도 할 수 없는 기능 입니다."),
+  SUCCESS_UPDATE_SUGGESTION_ANSWER("답변 수정이 완료 되었습니다."),
+  BAD_UPDATE_SUGGESTION_ANSWER("답변 수정이 실패 하였습니다. 대표 부대표만 가능합니다.");
 
   private final String message;
 
--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/ClubArticleController.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/controller/ClubArticleController.java
@@ -184,6 +184,30 @@ public class ClubArticleController {
         ResponseMessage.BAD_SUGGESTION_ANSWER.get());
   }
 
+  @Operation(summary = "팀 건의 게시판 - 상세 페이지 - 답변 쓰기", description = "팀 건의 게시판 답변 쓰기")
+  @PutMapping(value = "/{article_id}/answer/write")
+  public ResponseEntity<BaseResponseBody> updateSuggestionAnswerClubArticle(
+      HttpServletRequest request,
+      @RequestBody @Valid WriteSuggestionAnswerReqDto writeSuggestionAnswerReqDto,
+      @PathVariable(value = "club_id") Long clubId,
+      @PathVariable(value = "article_id") Long articleId) {
+    Long memberId = tokenService.getMemberIdFromToken(request);
+
+    Boolean isClubLeader = clubGradeService.isMemberStatus(clubId, memberId, CLUB_GRADE.LEADER);
+    Boolean isClubDeputyLeader = clubGradeService.isMemberStatus(clubId, memberId,
+        CLUB_GRADE.DEPUTY_LEADER);
+
+    // 클럽의 대표, 부대표 확인
+    if (isClubLeader || isClubDeputyLeader) {
+      clubArticleService.updateSuggestionAnswerClubArticle(articleId, writeSuggestionAnswerReqDto);
+
+      return BaseResponseBodyUtiil.BaseResponseBodySuccess(
+          ResponseMessage.SUCCESS_UPDATE_SUGGESTION_ANSWER.get());
+    }
+    return BaseResponseBodyUtiil.BaseResponseBodyFailure(
+        ResponseMessage.BAD_UPDATE_SUGGESTION_ANSWER.get());
+  }
+
 
   @Operation(summary = "팀 게시판 - 상세 페이지 - 댓글 쓰기", description = "팀 게시판 - 상세 페이지 - 댓글 쓰기")
   @PostMapping(value = "/{article_id}/comment/write")
