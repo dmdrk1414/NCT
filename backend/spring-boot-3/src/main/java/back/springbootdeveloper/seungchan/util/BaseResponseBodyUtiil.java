8f974cf034fc2bfc1927268ffcfa4789c42c3a68
박승찬 <seungchan141414@gmail.com>
2024-03-03 17:52:59 +0900

    [Feat : back] 기본 Response 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/util/BaseResponseBodyUtiil.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/util/BaseResponseBodyUtiil.java
@@ -9,39 +9,51 @@ import java.time.ZonedDateTime;
 
 public class BaseResponseBodyUtiil {
 
-    public static ResponseEntity<BaseResponseBody> BaseResponseBodySuccess() {
-        return new ResponseEntity<>(
-                BaseResponseBody.builder()
-                        .message("SUCCESS")
-                        .httpStatus(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .timestamp(ZonedDateTime.now(ZoneId.of("Z")))
-                        .build(),
-                HttpStatus.OK
-        );
-    }
+  public static ResponseEntity<BaseResponseBody> BaseResponseBodySuccess() {
+    return new ResponseEntity<>(
+        BaseResponseBody.builder()
+            .message("SUCCESS")
+            .httpStatus(HttpStatus.OK)
+            .statusCode(HttpStatus.OK.value())
+            .timestamp(ZonedDateTime.now(ZoneId.of("Z")))
+            .build(),
+        HttpStatus.OK
+    );
+  }
 
-    public static ResponseEntity<BaseResponseBody> BaseResponseBodySuccess(String message) {
-        return new ResponseEntity<>(
-                BaseResponseBody.builder()
-                        .message(message)
-                        .httpStatus(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .timestamp(ZonedDateTime.now(ZoneId.of("Z")))
-                        .build(),
-                HttpStatus.OK
-        );
-    }
+  public static ResponseEntity<BaseResponseBody> BaseResponseBodySuccess(String message) {
+    return new ResponseEntity<>(
+        BaseResponseBody.builder()
+            .message(message)
+            .httpStatus(HttpStatus.OK)
+            .statusCode(HttpStatus.OK.value())
+            .timestamp(ZonedDateTime.now(ZoneId.of("Z")))
+            .build(),
+        HttpStatus.OK
+    );
+  }
 
-    public static ResponseEntity<BaseResponseBody> BaseResponseBodyForbidden() {
-        return new ResponseEntity<>(
-                BaseResponseBody.builder()
-                        .message("Access denied")
-                        .httpStatus(HttpStatus.FORBIDDEN)
-                        .statusCode(HttpStatus.FORBIDDEN.value())
-                        .timestamp(ZonedDateTime.now(ZoneId.of("Z")))
-                        .build(),
-                HttpStatus.FORBIDDEN
-        );
-    }
+  public static ResponseEntity<BaseResponseBody> BaseResponseBodyForbidden() {
+    return new ResponseEntity<>(
+        BaseResponseBody.builder()
+            .message("Access denied")
+            .httpStatus(HttpStatus.FORBIDDEN)
+            .statusCode(HttpStatus.FORBIDDEN.value())
+            .timestamp(ZonedDateTime.now(ZoneId.of("Z")))
+            .build(),
+        HttpStatus.FORBIDDEN
+    );
+  }
+
+  public static ResponseEntity<BaseResponseBody> BaseResponseBodyBad(String message) {
+    return new ResponseEntity<>(
+        BaseResponseBody.builder()
+            .message(message)
+            .httpStatus(HttpStatus.BAD_REQUEST)
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .timestamp(ZonedDateTime.now(ZoneId.of("Z")))
+            .build(),
+        HttpStatus.OK
+    );
+  }
 }
