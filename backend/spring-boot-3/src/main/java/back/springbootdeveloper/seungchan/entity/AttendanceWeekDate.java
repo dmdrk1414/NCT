ab1a9114aa8865b0360ea25462122f96b6d777dc
박승찬 <seungchan141414@gmail.com>
2024-03-04 18:43:42 +0900

    [Feat : back] AttendanceWeekDate의 정보를 저장하기위한 유틸 메서드 생성

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/entity/AttendanceWeekDate.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/entity/AttendanceWeekDate.java
@@ -2,6 +2,7 @@ package back.springbootdeveloper.seungchan.entity;
 
 
 import back.springbootdeveloper.seungchan.constant.entity.ATTENDANCE_STATE;
+import back.springbootdeveloper.seungchan.domain.AttendanceStateList;
 import jakarta.persistence.*;
 import java.util.List;
 import lombok.Builder;
@@ -63,14 +64,13 @@ public class AttendanceWeekDate {
 
 
   @Builder
-  public AttendanceWeekDate(ATTENDANCE_STATE monday, ATTENDANCE_STATE tuesday,
-      ATTENDANCE_STATE wednesday, ATTENDANCE_STATE thursday, ATTENDANCE_STATE friday
-  ) {
-    this.monday = monday;
-    this.tuesday = tuesday;
-    this.wednesday = wednesday;
-    this.thursday = thursday;
-    this.friday = friday;
+  public AttendanceWeekDate(AttendanceStateList attendanceStateList) {
+    this.userId = attendanceStateList.getUserId();
+    this.monday = attendanceStateList.getAttendanceStates().get(0);
+    this.tuesday = attendanceStateList.getAttendanceStates().get(1);
+    this.wednesday = attendanceStateList.getAttendanceStates().get(2);
+    this.thursday = attendanceStateList.getAttendanceStates().get(3);
+    this.friday = attendanceStateList.getAttendanceStates().get(4);
   }
 
   @PrePersist
@@ -94,141 +94,6 @@ public class AttendanceWeekDate {
     this.friday = ATTENDANCE_STATE.UNDECIDED;
   }
 
-  /**
-   * 오늘의 출석 상태를 업데이트합니다.
-   */
-  public void updateAttendanceAtToday() {
-    ZonedDateTime now = ZonedDateTime.now();
-    DayOfWeek currentDayOfWeek = now.getDayOfWeek();
-
-    switch (currentDayOfWeek) {
-      case MONDAY:
-        this.monday = ATTENDANCE_STATE.ATTENDANCE;
-        break;
-      case TUESDAY:
-        this.tuesday = ATTENDANCE_STATE.ATTENDANCE;
-        break;
-      case WEDNESDAY:
-        this.wednesday = ATTENDANCE_STATE.ATTENDANCE;
-        break;
-      case THURSDAY:
-        this.thursday = ATTENDANCE_STATE.ATTENDANCE;
-        break;
-      case FRIDAY:
-        this.friday = ATTENDANCE_STATE.ATTENDANCE;
-        break;
-      default:
-        break;
-    }
-  }
-
-  /**
-   * 오늘의 결석을 업데이트합니다.
-   */
-  public void updateAbsenceAtToday() {
-    ZonedDateTime now = ZonedDateTime.now();
-    DayOfWeek currentDayOfWeek = now.getDayOfWeek();
-
-    switch (currentDayOfWeek) {
-      case MONDAY:
-        this.monday = ATTENDANCE_STATE.ABSENCE;
-        break;
-      case TUESDAY:
-        this.tuesday = ATTENDANCE_STATE.ABSENCE;
-        break;
-      case WEDNESDAY:
-        this.wednesday = ATTENDANCE_STATE.ABSENCE;
-        break;
-      case THURSDAY:
-        this.thursday = ATTENDANCE_STATE.ABSENCE;
-        break;
-      case FRIDAY:
-        this.friday = ATTENDANCE_STATE.ABSENCE;
-        break;
-      case SATURDAY:
-      default:
-        break;
-    }
-  }
-
-  /**
-   * 오늘의 휴가를 업데이트합니다.
-   */
-  public void updateVacationAtToday() {
-    ZonedDateTime now = ZonedDateTime.now();
-    DayOfWeek currentDayOfWeek = now.getDayOfWeek();
-
-    switch (currentDayOfWeek) {
-      case MONDAY:
-        this.monday = ATTENDANCE_STATE.VACATION;
-        break;
-      case TUESDAY:
-        this.tuesday = ATTENDANCE_STATE.VACATION;
-        break;
-      case WEDNESDAY:
-        this.wednesday = ATTENDANCE_STATE.VACATION;
-        break;
-      case THURSDAY:
-        this.thursday = ATTENDANCE_STATE.VACATION;
-        break;
-      case FRIDAY:
-        this.friday = ATTENDANCE_STATE.VACATION;
-        break;
-      default:
-        break;
-    }
-  }
-
-  /**
-   * 요일에 따른 출석 상태를 반환합니다.
-   *
-   * @param dayOfWeek 요일
-   * @return 해당 요일의 출석 상태
-   */
-  public ATTENDANCE_STATE getAttendanceStateForDay(DayOfWeek dayOfWeek) {
-    switch (dayOfWeek) {
-      case MONDAY:
-        return this.monday;
-      case TUESDAY:
-        return this.tuesday;
-      case WEDNESDAY:
-        return this.wednesday;
-      case THURSDAY:
-        return this.thursday;
-      case FRIDAY:
-        return this.friday;
-      default:
-        break;
-    }
-    return null;
-  }
-
-  /**
-   * 현재 요일에 따라 출석 상태를 업데이트할 수 있는지 여부를 확인합니다.
-   *
-   * @return 출석 상태를 업데이트할 수 있는 경우 true, 그렇지 않으면 false를 반환합니다.
-   */
-  public Boolean isPossibleUpdateAttendanceState() {
-    // 현재 시간을 기준으로 요일을 가져옵니다.
-    ZonedDateTime now = ZonedDateTime.now();
-    DayOfWeek currentDayOfWeek = now.getDayOfWeek();
-
-    // 요일에 따라 처리를 수행합니다.
-    switch (currentDayOfWeek) {
-      case MONDAY:
-        return this.monday.is(ATTENDANCE_STATE.UNDECIDED);
-      case TUESDAY:
-        return this.tuesday.is(ATTENDANCE_STATE.UNDECIDED);
-      case WEDNESDAY:
-        return this.wednesday.is(ATTENDANCE_STATE.UNDECIDED);
-      case THURSDAY:
-        return this.thursday.is(ATTENDANCE_STATE.UNDECIDED);
-      case FRIDAY:
-        return this.friday.is(ATTENDANCE_STATE.UNDECIDED);
-    }
-    // 기타 경우에는 업데이트가 불가능하므로 false를 반환합니다.
-    return false;
-  }
 
   public List<ATTENDANCE_STATE> getAllAttendanceWeekDates() {
     return List.of(
