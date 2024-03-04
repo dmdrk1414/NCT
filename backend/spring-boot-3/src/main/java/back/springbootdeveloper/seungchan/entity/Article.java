b95d0c77769be0470d608e60a70f09f52fbc1d1b
박승찬 <seungchan141414@gmail.com>
2024-03-05 04:37:21 +0900

    [Style : back] 주석 추가

--- backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/entity/Article.java
+++ backend/spring-boot-3/src/main/java/back/springbootdeveloper/seungchan/entity/Article.java
@@ -52,30 +52,52 @@ public class Article extends BaseEntity {
     this.articleDate = zonedDateTime.toLocalDate();
   }
 
+  /**
+   * 글의 제목을 업데이트하는 메서드입니다.
+   *
+   * @param title 새로운 제목
+   */
   public void updateTitle(String title) {
     this.title = title;
   }
 
+  /**
+   * 글의 내용을 업데이트하는 메서드입니다.
+   *
+   * @param content 새로운 내용
+   */
   public void updateContent(String content) {
     this.content = content;
   }
 
+  /**
+   * 신고 수를 감소시키는 메서드입니다. 만약 신고 수가 0보다 큰 경우에만 감소시킵니다.
+   */
   public void subtractDeclaration() {
     if (this.declarationCount > 0) {
       this.declarationCount = this.declarationCount - 1;
     }
   }
 
+  /**
+   * 신고 수를 증가시키는 메서드입니다.
+   */
   public void addDeclaration() {
     this.declarationCount = this.declarationCount + 1;
   }
 
+  /**
+   * 좋아요 수를 감소시키는 메서드입니다. 만약 좋아요 수가 0보다 큰 경우에만 감소시킵니다.
+   */
   public void subtractLike() {
     if (this.likeCount > 0) {
       this.likeCount = this.likeCount - 1;
     }
   }
 
+  /**
+   * 좋아요 수를 증가시키는 메서드입니다.
+   */
   public void addLike() {
     this.likeCount = this.likeCount + 1;
   }
