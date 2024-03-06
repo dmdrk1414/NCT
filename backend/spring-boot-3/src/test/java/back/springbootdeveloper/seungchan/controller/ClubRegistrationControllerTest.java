c07d95afb728154c4ca53405cb9067323a145cfa
박승찬 <seungchan141414@gmail.com>
2024-03-06 22:18:20 +0900

    [Test : back] 팀이름_중복_검사_예외_중복_테스트

--- backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/ClubRegistrationControllerTest.java
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/controller/ClubRegistrationControllerTest.java
@@ -111,4 +111,35 @@ class ClubRegistrationControllerTest {
         .andExpect(jsonPath("$.message").value(
             RESPONSE_MESSAGE_VALUE.SUCCESS_NOT_DUPLICATION_CLUBNAME(targetClubName)));
   }
+
+  @Test
+  void 팀이름_중복_검사_예외_중복_테스트() throws Exception {
+    // given
+    // 유저 로그인
+    final String token = testCreateUtil.create_token_one_club_leader_member();
+    final String url = "/main/club/apply/overlap";
+    final Member targetMember = memberOneClubLeader;
+
+    // 검증 준비
+    final String targetClubName = clubRepository.findAll().get(0).getClubName();
+    final CheckDuplicationClubNameReqDto requestDto = CheckDuplicationClubNameReqDto.builder()
+        .clubName(targetClubName)
+        .build();
+
+    // when
+    final String requestBody = objectMapper.writeValueAsString(requestDto);
+    ResultActions result = mockMvc.perform(
+        post(url)
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(requestBody)
+            .header("authorization", "Bearer " + token) // token header에 담기
+    );
+
+    // then
+    result
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.message").value(
+            ResponseMessage.BAD_DUPLICATION_CLUBNAME.get()));
+  }
 }
