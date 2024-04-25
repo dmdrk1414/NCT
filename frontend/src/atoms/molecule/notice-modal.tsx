fd34ecd1d73a6a5a1564c7258fd5e37e8f78f160
박승찬 <seungchan141414@gmail.com>
2024-04-26 00:41:05 +0900

    [Style : front] 공지사항 버튼 css 수정

--- frontend/src/atoms/molecule/notice-modal.tsx
+++ frontend/src/atoms/molecule/notice-modal.tsx
@@ -52,12 +52,12 @@ export default function NoticeModal() {
   };
 
   return (
-    <ul className="notice overflow-y-auto h-[1.5rem]  bg-[#4b89cc4b] font-medium mb-[0.5rem]" onClick={() => handleItemClick()}>
+    <ul className="notice overflow-y-auto h-[3.2rem] rounded-md bg-[#4b89cc4b] font-medium mb-[0.5rem]" onClick={() => handleItemClick()}>
       <Slider {...settings} className="h-[1.5rem]">
         {noticeInformations.map((item, idx) => (
           <li className="notice_content" key={idx}>
-            <div>
-              <span className="notice_header">공지 : </span>
+            <div className="truncate">
+              <span className="notice_header">* </span>
               <span className="notice_title">{item.title}</span>
             </div>
           </li>
