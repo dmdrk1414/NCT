1900c0cc840e9d3957a92a383e54dd4ade494230
dmdrk1414 <seungchan141414@gmail.com>
2024-05-04 01:30:58 +0900

    [Test : back] Test 전용 커스텀 에너테이션
    모든 테스트 클래스 상단에 사용을 위한 커스텀 에너테이션 추가

---
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/testutills/TestSaveEntity.java
@@ -0,0 +1,16 @@
+package back.springbootdeveloper.seungchan.testutills;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestSaveEntity {
+
+
+  public void creatEntityTest() throws Exception {
+  }
+
+  void 테스트_데이터_베이스_저장(Integer clubNumber, Integer startMemberNumber,
+      Integer endMemberNumber) throws Exception {
+
+    }
+}
