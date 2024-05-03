9381b55950dc440c36d1a7a3f16b5f859f455cc9
dmdrk1414 <seungchan141414@gmail.com>
2024-05-04 02:36:36 +0900

    [Test : back] Test Save Notice Entity 추가

--- backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/testutills/TestSaveEntity.java
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/testutills/TestSaveEntity.java
@@ -1,16 +1,38 @@
 package back.springbootdeveloper.seungchan.testutills;
 
+import back.springbootdeveloper.seungchan.entity.Notice;
+import back.springbootdeveloper.seungchan.repository.NoticeRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TestSaveEntity {
 
+  @Autowired
+  private NoticeRepository noticeRepository;
 
   public void creatEntityTest() throws Exception {
+    // Notice Entity
+    createNoticeEntity();
+
   }
 
-  void 테스트_데이터_베이스_저장(Integer clubNumber, Integer startMemberNumber,
-      Integer endMemberNumber) throws Exception {
-
+  /**
+   * Notice Entity
+   */
+  private void createNoticeEntity() {
+    List<Notice> notices = new ArrayList<>();
+    for (int i = 1; i <= 100; i++) {
+      notices.add(
+          Notice.builder()
+              .title("테스트 공지사항 제목_" + i)
+              .content("테스트 공지사항 내용_" + i)
+              .build()
+      );
     }
+
+    noticeRepository.saveAll(notices);
+  }
 }
