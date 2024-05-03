828f1b482459a16f9d24d360098c6d465d5bf38f
dmdrk1414 <seungchan141414@gmail.com>
2024-05-04 02:36:46 +0900

    [Test : back] Test

--- backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/domain/TemporaryPasswordTest.java
+++ backend/spring-boot-3/src/test/java/back/springbootdeveloper/seungchan/domain/TemporaryPasswordTest.java
@@ -1,35 +1,48 @@
 package back.springbootdeveloper.seungchan.domain;
 
+import back.springbootdeveloper.seungchan.annotation.NctSpringBootTest;
 import back.springbootdeveloper.seungchan.constant.regexp.RegexpConstant;
+import back.springbootdeveloper.seungchan.repository.NoticeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@NctSpringBootTest
 class TemporaryPasswordTest {
 
-    @BeforeEach
-    void setUp() {
+  @Autowired
+  private NoticeRepository noticeRepository;
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void 커스텀() {
+    System.out.println("noticeRepository.count() = " + noticeRepository.count());
+  }
+
+
+  @Test
+  void 비밀번호_형식에_맞는_임시_비밀번호_생성_확인_테스트() {
+    // given
+    TemporaryPassword temporaryPassword = new TemporaryPassword();
+    String regexPassword = RegexpConstant.PASSWORD.get();
+
+    // when
+    // then
+    for (int i = 0; i < 10000; i++) {
+      Boolean result = true;
+
+      String tempPasswordString = temporaryPassword.generate();
+      if (!tempPasswordString.matches(regexPassword)) {
+        result = false;
+      }
+
+      assertThat(result).isTrue();
     }
-
-    @Test
-    void 비밀번호_형식에_맞는_임시_비밀번호_생성_확인_테스트() {
-        // given
-        TemporaryPassword temporaryPassword = new TemporaryPassword();
-        String regexPassword = RegexpConstant.PASSWORD.get();
-
-        // when
-        // then
-        for (int i = 0; i < 10000; i++) {
-            Boolean result = true;
-
-            String tempPasswordString = temporaryPassword.generate();
-            if (!tempPasswordString.matches(regexPassword)) {
-                result = false;
-            }
-
-            assertThat(result).isTrue();
-        }
-    }
+  }
 }
